deskapp/otrs-ib

commit 407f071ee51b80311b4f0e57a64c2fd9333b886d

Added ExtFQDN option, parameter name fixed, internal data removed from some Message-IDs
OTRS uses the fully qualified domain name (FQDN SysConfig setting)
for external IDs generation (i.e. Message-ID in e-mail messages,
ContentID for pasted images). If OTRS instance uses internal
domain for FQDN, this internal domain name will leak to third
parties in e-mail message headers which may be not desirable.

This mod introduces new SysConfig option ExtFQDN which allowes
one to define the fully qualified domain name for external IDs
generation (i.e. Message-ID in e-mail messages, Content-ID for
pasted images).

It also fixes parameter name in Bounce() call and removes UserID
and TicketID from some Message-IDs generated by OTRS (this
is internal data and it's not necessary to expose it to third
parties in e-mail message headers).

Related: https://dev.ib.pl/ib/otrs/issues/40
Related: http://bugs.otrs.org/show_bug.cgi?id=9003
Author-Change-Id: IB#1009570

Kernel/Config/Files/Framework.xml
@@ -80,6 +80,14 @@
             <String Regex="">yourhost.example.com</String>
         </Setting>
     </ConfigItem>
+    <ConfigItem Name="ExtFQDN" Required="1" Valid="1" ConfigLevel="200">
+        <Description Translatable="1">Defines the fully qualified domain name for external IDs generation (i.e. Message-ID).</Description>
+        <Group>Framework</Group>
+        <SubGroup>Core</SubGroup>
+        <Setting>
+            <String Regex="">yourexternal.address.com</String>
+        </Setting>
+    </ConfigItem>
     <ConfigItem Name="NodeID" Required="0" Valid="0" ConfigLevel="200">
         <Description Translatable="1">Defines the cluster node identifier. This is only used in cluster configurations where there is more than one OTRS frontend system. Note: only values from 1 to 99 are allowed.</Description>
         <Group>Framework</Group>

Kernel/System/Email.pm
@@ -899,9 +899,9 @@ sub _EncodeMIMEWords {
 sub _MessageIDCreate {
     my ( $Self, %Param ) = @_;
 
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
 
-    return 'Message-ID: <' . time() . '.' . rand(999999) . '@' . $FQDN . '>';
+    return 'Message-ID: <' . time() . '.' . rand(999999) . '@' . $ExtFQDN . '>';
 }
 
 1;

Kernel/System/HTMLUtils.pm
@@ -1189,14 +1189,14 @@ sub EmbeddedImagesExtract {
         return;
     }
 
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
     ${ $Param{DocumentRef} } =~ s{(src=")(data:image/)(png|gif|jpg|jpeg|bmp)(;base64,)(.+?)(")}{
 
         my $Base64String = $5;
 
         my $FileName     = 'pasted-' . time() . '-' . int(rand(1000000)) . '.' . $3;
         my $ContentType  = "image/$3; name=\"$FileName\"";
-        my $ContentID    = 'pasted.' . time() . '.' . int(rand(1000000)) . '@' . $FQDN;
+        my $ContentID    = 'pasted.' . time() . '.' . int(rand(1000000)) . '@' . $ExtFQDN;
 
         my $AttachmentData = {
             Content     => decode_base64($Base64String),

Kernel/System/Ticket/Article.pm
@@ -2236,8 +2236,8 @@ sub ArticleSend {
     # create article
     my $Time      = $Kernel::OM->Get('Kernel::System::Time')->SystemTime();
     my $Random    = rand 999999;
-    my $FQDN      = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
-    my $MessageID = "<$Time.$Random.$Param{TicketID}.$Param{UserID}\@$FQDN>";
+    my $ExtFQDN   = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+    my $MessageID = "<$Time.$Random\@$ExtFQDN>";
     my $ArticleID = $Self->ArticleCreate(
         %Param,
         MessageID => $MessageID,
@@ -2321,8 +2321,8 @@ sub ArticleBounce {
     # create message id
     my $Time         = $Kernel::OM->Get('Kernel::System::Time')->SystemTime();
     my $Random       = rand 999999;
-    my $FQDN         = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
-    my $NewMessageID = "<$Time.$Random.$Param{TicketID}.0.$Param{UserID}\@$FQDN>";
+    my $ExtFQDN      = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+    my $NewMessageID = "<$Time.$Random.0\@$ExtFQDN>";
     my $Email        = $Self->ArticlePlain( ArticleID => $Param{ArticleID} );
 
     # check if plain email exists
@@ -2336,10 +2336,10 @@ sub ArticleBounce {
 
     # pipe all into sendmail
     return if !$Kernel::OM->Get('Kernel::System::Email')->Bounce(
-        MessageID => $NewMessageID,
-        From      => $Param{From},
-        To        => $Param{To},
-        Email     => $Email,
+        'Message-ID' => $NewMessageID,
+        From         => $Param{From},
+        To           => $Param{To},
+        Email        => $Email,
     );
 
     # write history

Kernel/System/Web/UploadCache/DB.pm
@@ -96,9 +96,9 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $FQDN   = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+        my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
 
-        $ContentID = "$Disposition$Random.$Param{FormID}\@$FQDN";
+        $ContentID = "$Disposition$Random.$Param{FormID}\@$ExtFQDN";
     }
 
     # write attachment to db

Kernel/System/Web/UploadCache/FS.pm
@@ -91,9 +91,9 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $FQDN   = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+        my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
 
-        $ContentID = "$Disposition$Random.$Param{FormID}\@$FQDN";
+        $ContentID = "$Disposition$Random.$Param{FormID}\@$ExtFQDN";
     }
 
     # get main object

commit 20141c74307dee6339757d2b9ad3451a6a18ab6a

Make ExtFQDN optional
This makes ExtFQDN introduced in 407f071ee51b80311b4f0e57a64c2fd9333b886d
optional. If ExtFQDN is not defined in SysConfig, OTRS will use FQDN setting
for Message-ID and ContentID generation.

Fixes: 407f071ee51b80311b4f0e57a64c2fd9333b886d
Related: https://dev.ib.pl/ib/otrs/issues/40
Related: http://bugs.otrs.org/show_bug.cgi?id=9003
Author-Change-Id: IB#1009570

Kernel/Config/Files/Framework.xml
@@ -80,8 +80,8 @@
             <String Regex="">yourhost.example.com</String>
         </Setting>
     </ConfigItem>
-    <ConfigItem Name="ExtFQDN" Required="1" Valid="1" ConfigLevel="200">
-        <Description Translatable="1">Defines the fully qualified domain name for external IDs generation (i.e. Message-ID).</Description>
+    <ConfigItem Name="ExtFQDN" Required="0" Valid="0" ConfigLevel="200">
+        <Description Translatable="1">Defines the fully qualified domain name for external IDs generation (i.e. Message-ID, ContentID).</Description>
         <Group>Framework</Group>
         <SubGroup>Core</SubGroup>
         <Setting>

Kernel/System/Email.pm
@@ -899,7 +899,8 @@ sub _EncodeMIMEWords {
 sub _MessageIDCreate {
     my ( $Self, %Param ) = @_;
 
-    my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
 
     return 'Message-ID: <' . time() . '.' . rand(999999) . '@' . $ExtFQDN . '>';
 }

Kernel/System/HTMLUtils.pm
@@ -1189,7 +1189,8 @@ sub EmbeddedImagesExtract {
         return;
     }
 
-    my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
     ${ $Param{DocumentRef} } =~ s{(src=")(data:image/)(png|gif|jpg|jpeg|bmp)(;base64,)(.+?)(")}{
 
         my $Base64String = $5;

Kernel/System/Ticket/Article.pm
@@ -2233,10 +2233,12 @@ sub ArticleSend {
         AttachmentsRef => $Param{Attachment},
     );
 
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # create article
     my $Time      = $Kernel::OM->Get('Kernel::System::Time')->SystemTime();
     my $Random    = rand 999999;
-    my $ExtFQDN   = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+    my $ExtFQDN   = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
     my $MessageID = "<$Time.$Random\@$ExtFQDN>";
     my $ArticleID = $Self->ArticleCreate(
         %Param,
@@ -2318,10 +2320,12 @@ sub ArticleBounce {
         }
     }
 
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # create message id
     my $Time         = $Kernel::OM->Get('Kernel::System::Time')->SystemTime();
     my $Random       = rand 999999;
-    my $ExtFQDN      = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+    my $ExtFQDN      = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
     my $NewMessageID = "<$Time.$Random.0\@$ExtFQDN>";
     my $Email        = $Self->ArticlePlain( ArticleID => $Param{ArticleID} );
 

Kernel/System/Web/UploadCache/DB.pm
@@ -96,7 +96,8 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+        my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
 
         $ContentID = "$Disposition$Random.$Param{FormID}\@$ExtFQDN";
     }

Kernel/System/Web/UploadCache/FS.pm
@@ -91,7 +91,8 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $ExtFQDN = $Kernel::OM->Get('Kernel::Config')->Get('ExtFQDN');
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+        my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
 
         $ContentID = "$Disposition$Random.$Param{FormID}\@$ExtFQDN";
     }