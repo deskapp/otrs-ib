27c68a1ee65be8eab96fd8275ff1826517c6dcb9
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2016-05-02 13:14:59 +0200

    Merge master into master-ib

--- Kernel/Modules/AgentTicketForward.pm
+++ Kernel/Modules/AgentTicketForward.pm
@@ -1370,21 +1370,8 @@ sub _Mask {
         }
 
         my %Selected;
-        if ( $Param{ArticleTypeID} ) {
-
-            # use email-internal article type if forwarding article with internal type; otherwise use email-internal
-            my $ArticleTypeSelected = $TicketObject->ArticleTypeLookup( ArticleTypeID => $Param{ArticleTypeID} );
-            if ( $ArticleTypeSelected ) {
-                if ( $ArticleTypeSelected =~ m{internal} ) {
-                    $Selected{SelectedID} = $TicketObject->ArticleTypeLookup( ArticleType => 'email-internal' );
-                }
-                else {
-                    $Selected{SelectedID} = $TicketObject->ArticleTypeLookup( ArticleType => 'email-external' );
-                }
-            }
-            else {
-                $Selected{SelectedValue} = $Config->{DefaultArticleType};
-            }
+        if ( $Self->{GetParam}->{ArticleTypeID} ) {
+            $Selected{SelectedID} = $Self->{GetParam}->{ArticleTypeID};
         }
         else {
             $Selected{SelectedValue} = $Config->{ArticleTypeDefault};
