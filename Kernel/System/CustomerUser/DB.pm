ae9c5445d38cd22627c553c9481f096690d9c1cf
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2016-06-23 14:18:13 +0200

    Configurable bcrypt cost
    OTRS uses hardcoded bcrypt cost 9. This mod introduces new SysConfig
    parameter `AuthModule::DB::bcryptCost` that allowes one to tune bcrypt
    cost in range from 9 to 31 (31 seems to be Crypt::Eksblowfish::Bcrypt
    limit) and changes default cost from 9 to 12 for better security.
    
    Related: https://dev.ib.pl/ib/otrs/issues/78
    Author-Change-Id: IB#1023732

--- Kernel/System/CustomerUser/DB.pm
+++ Kernel/System/CustomerUser/DB.pm
@@ -992,6 +992,9 @@ sub SetPassword {
     my $Login = $Param{UserLogin};
     my $Pw = $Param{PW} || '';
 
+    # get config object
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # check ro/rw
     if ( $Self->{ReadOnly} ) {
         $Kernel::OM->Get('Kernel::System::Log')->Log(
@@ -1012,7 +1015,7 @@ sub SetPassword {
     my $CryptedPw = '';
 
     # get crypt type
-    my $CryptType = $Kernel::OM->Get('Kernel::Config')->Get('Customer::AuthModule::DB::CryptType') || 'sha2';
+    my $CryptType = $ConfigObject->Get('Customer::AuthModule::DB::CryptType') || 'sha2';
 
     # get encode object
     my $EncodeObject = $Kernel::OM->Get('Kernel::System::Encode');
@@ -1082,7 +1085,12 @@ sub SetPassword {
             return;
         }
 
-        my $Cost = 9;
+        # get cost from config; use 12 if not configured; don't allow values smaller than 9 for security;
+        # current Crypt::Eksblowfish::Bcrypt limit is 31
+        my $Cost = $ConfigObject->Get('Customer::AuthModule::DB::bcryptCost') // 12;
+        $Cost = 9 if $Cost < 9;
+        $Cost = 31 if $Cost > 31;
+
         my $Salt = $MainObject->GenerateRandomString( Length => 16 );
 
         # remove UTF8 flag, required by Crypt::Eksblowfish::Bcrypt
@@ -1092,7 +1100,7 @@ sub SetPassword {
         my $Octets = Crypt::Eksblowfish::Bcrypt::bcrypt_hash(
             {
                 key_nul => 1,
-                cost    => 9,
+                cost    => $Cost,
                 salt    => $Salt,
             },
             $Pw
