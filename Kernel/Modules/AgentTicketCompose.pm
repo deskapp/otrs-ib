deskapp/otrs-ib

commit e4b1555ba9515d0a46a860564c70c50edb3b1230

Simple ticket printout attaching
Added feature to attach ticket printout with one click
in AgentTicketCompose and AgentTicketForward.

May be useful for quick manual ticket forwarding to external
parties via e-mail. Use with caution - ticket internal
articles are also included in printout.

To enable this feature, switch on one or both new parameters
in SysConfig:

    Ticket::Frontend::AgentTicketCompose###AttachTicketPrintout
    Ticket::Frontend::AgentTicketForward###AttachTicketPrintout

After enabling extra button `Attach ticket printout` will be
available under the attachment upload button.

Related: https://dev.ib.pl/ib/otrs/issues/3
Related: https://github.com/OTRS/otrs/pull/266
Author-Change-Id: IB#1027611

Kernel/Modules/AgentTicketCompose.pm
@@ -514,6 +514,48 @@ sub Run {
             );
         }
 
+        # ticket number lookup
+        my $Tn = $TicketObject->TicketNumberLookup( TicketID => $Self->{TicketID} );
+
+        # attachment add ticket printout if enabled
+        if ( $Config->{AttachTicketPrintout} ) {
+            if ( $ParamObject->GetParam( Param => 'AttachmentAddTicketPrintout' ) ) {
+                %Error = ();
+                $Error{AttachmentAddTicketPrintout} = 1;
+
+                # assemble file name
+                my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
+                if ( $Self->{UserTimeZone} ) {
+                    $DateTimeObject->ToTimeZone( TimeZone => $Self->{UserTimeZone} );
+                }
+                my $Filename = 'Ticket_' . $Tn . '_';
+                $Filename .= $DateTimeObject->Format( Format => '%Y-%m-%d_%H:%M' );
+                $Filename .= '.pdf';
+
+                # generate pdf printout
+                my $PDFString = $Kernel::OM->Get('Kernel::Output::PDF::Ticket')->GenerateAgentPDF(
+                    TicketID => $Self->{TicketID},
+                    UserID   => $Self->{UserID},
+                );
+
+                if ( $PDFString ) {
+                    $UploadCacheObject->FormIDAddFile(
+                        FormID => $GetParam{FormID},
+                        Disposition => 'attachment',
+                        Filename    => $Filename,
+                        Content     => $PDFString,
+                        ContentType => 'application/pdf',
+                    );
+                 }
+                 else {
+                    $Kernel::OM->Get('Kernel::System::Log')->Log(
+                        Priority => 'error',
+                        Message  => 'Error attaching ticket printout on compose (ticketid=' . $Self->{TicketID} . ')',
+                    );
+                 }
+             }
+         }
+
         # get all attachments meta data
         my @Attachments = $UploadCacheObject->FormIDGetAllFilesMeta(
             FormID => $Self->{FormID},
@@ -583,7 +625,6 @@ sub Run {
         }
 
         # prepare subject
-        my $Tn = $TicketObject->TicketNumberLookup( TicketID => $Self->{TicketID} );
         $GetParam{Subject} = $TicketObject->TicketSubjectBuild(
             TicketNumber => $Tn,
             Subject      => $GetParam{Subject} || '',
@@ -2038,6 +2079,14 @@ sub _Mask {
         $LayoutObject->Block(
             Name => 'Attachment',
             Data => $Attachment,
+        );
+    }
+
+    # show add printout attachment button if enabled
+    if ( $Config->{AttachTicketPrintout} ) {
+        $LayoutObject->Block(
+            Name => 'AttachmentAddTicketPrintoutButton',
+            Data => \%Param,
         );
     }
 

Kernel/Modules/AgentTicketForward.pm
@@ -894,6 +894,47 @@ sub SendEmail {
         );
     }
 
+    # ticket number lookup
+    my $Tn = $TicketObject->TicketNumberLookup( TicketID => $Self->{TicketID} );
+
+    if ( $Config->{AttachTicketPrintout} ) {
+        if ( $ParamObject->GetParam( Param => 'AttachmentAddTicketPrintout' ) ) {
+            %Error = ();
+            $Error{AttachmentAddTicketPrintout} = 1;
+
+            # assemble file name
+            my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
+            if ( $Self->{UserTimeZone} ) {
+                $DateTimeObject->ToTimeZone( TimeZone => $Self->{UserTimeZone} );
+            }
+            my $Filename = 'Ticket_' . $Tn . '_';
+            $Filename .= $DateTimeObject->Format( Format => '%Y-%m-%d_%H:%M' );
+            $Filename .= '.pdf';
+
+            # generate pdf printout
+            my $PDFString = $Kernel::OM->Get('Kernel::Output::PDF::Ticket')->GenerateAgentPDF(
+                TicketID => $Self->{TicketID},
+                UserID   => $Self->{UserID},
+            );
+
+            if ( $PDFString ) {
+                $UploadCacheObject->FormIDAddFile(
+                    FormID => $GetParam{FormID},
+                    Disposition => 'attachment',
+                    Filename    => $Filename,
+                    Content     => $PDFString,
+                    ContentType => 'application/pdf',
+                );
+            }
+            else {
+                $Kernel::OM->Get('Kernel::System::Log')->Log(
+                   Priority => 'error',
+                   Message  => 'Error attaching ticket printout on forward (ticketid=' . $Self->{TicketID} . ')',
+                );
+            }
+        }
+    }
+
     # get all attachments meta data
     my @Attachments = $UploadCacheObject->FormIDGetAllFilesMeta(
         FormID => $GetParam{FormID},
@@ -1680,6 +1721,14 @@ sub _Mask {
         $LayoutObject->Block(
             Name => 'Attachment',
             Data => $Attachment,
+        );
+    }
+
+    # show add printout attachment button if enabled
+    if ( $Config->{AttachTicketPrintout} ) {
+        $LayoutObject->Block(
+            Name => 'AttachmentAddTicketPrintoutButton',
+            Data => \%Param,
         );
     }
 

Kernel/Config/Files/Ticket.xml
@@ -10785,6 +10785,17 @@ Thanks for your help!
             </Hash>
         </Setting>
     </ConfigItem>
+    <ConfigItem Name="Ticket::Frontend::AgentTicketCompose###AttachTicketPrintout" Required="1" Valid="1">
+        <Description Translatable="1">If enabled, simple ticket printout attaching will be available in the ticket compose screen of the agent interface.</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Frontend::Agent::Ticket::ViewCompose</SubGroup>
+        <Setting>
+            <Option SelectedID="0">
+                <Item Key="0">No</Item>
+                <Item Key="1">Yes</Item>
+            </Option>
+        </Setting>
+    </ConfigItem>
     <ConfigItem Name="Ticket::Frontend::AgentTicketEmail###DynamicField" Required="0" Valid="1">
         <Description Translatable="1">Dynamic fields shown in the ticket email screen of the agent interface. Possible settings: 0 = Disabled, 1 = Enabled, 2 = Enabled and required.</Description>
         <Group>Ticket</Group>
@@ -10810,6 +10821,17 @@ Thanks for your help!
         <Setting>
             <Hash>
             </Hash>
+        </Setting>
+    </ConfigItem>
+    <ConfigItem Name="Ticket::Frontend::AgentTicketForward###AttachTicketPrintout" Required="1" Valid="1">
+        <Description Translatable="1">If enabled, simple ticket printout attaching will be available in the ticket forward screen of the agent interface.</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Frontend::Agent::Ticket::ViewForward</SubGroup>
+        <Setting>
+            <Option SelectedID="0">
+                <Item Key="0">No</Item>
+                <Item Key="1">Yes</Item>
+            </Option>
         </Setting>
     </ConfigItem>
     <ConfigItem Name="Ticket::Frontend::AgentTicketEmailOutbound###DynamicField" Required="0" Valid="1">

Kernel/Output/HTML/Templates/Standard/AgentTicketCompose.tt
@@ -362,6 +362,23 @@
                                 <input id="FileUpload" name="FileUpload" type="file" size="40" />
                                 <input type="hidden" id="AttachmentUpload" name="AttachmentUpload" value="0" />
                             </li>
+[% RenderBlockStart("AttachmentAddTicketPrintoutButton") %]
+                            <li>
+                                [% Translate("Warning: printout will include internal information also!") | html %]
+                                <button type="button" id="AttachmentAddTicketPrintoutButton" name="AttachmentAddTicketPrintoutButton" value="AddTicketPrintout" class="SpacingLeft">[% Translate("Attach ticket printout") | html %]</button>
+                                <input type="hidden" id="AttachmentAddTicketPrintout" name="AttachmentAddTicketPrintout" />
+[% WRAPPER JSOnDocumentComplete %]
+<script type="text/javascript">//<![CDATA[
+     $('#AttachmentAddTicketPrintoutButton').bind('click', function () {
+         var $Form = $('#AttachmentAddTicketPrintoutButton').closest('form');
+         $('#AttachmentAddTicketPrintout').val(1);
+         Core.Form.Validate.DisableValidation($Form);
+         $Form.trigger('submit');
+     });
+//]]></script>
+[% END %]
+                            </li>
+[% RenderBlockEnd("AttachmentAddTicketPrintoutButton") %]
                         </ul>
 [% WRAPPER JSOnDocumentComplete %]
 <script type="text/javascript">//<![CDATA[

Kernel/Output/HTML/Templates/Standard/AgentTicketForward.tt
@@ -342,6 +342,23 @@
                             <input id="FileUpload" name="FileUpload" type="file" size="40" />
                             <input type="hidden" id="AttachmentUpload" name="AttachmentUpload" value="0" />
                         </li>
+[% RenderBlockStart("AttachmentAddTicketPrintoutButton") %]
+                        <li>
+                            [% Translate("Warning: printout will include internal information also!") | html %]
+                            <button type="button" id="AttachmentAddTicketPrintoutButton" name="AttachmentAddTicketPrintoutButton" value="AddTicketPrintout" class="SpacingLeft">[% Translate("Attach ticket printout") | html %]</button>
+                            <input type="hidden" id="AttachmentAddTicketPrintout" name="AttachmentAddTicketPrintout" />
+[% WRAPPER JSOnDocumentComplete %]
+<script type="text/javascript">//<![CDATA[
+     $('#AttachmentAddTicketPrintoutButton').bind('click', function () {
+         var $Form = $('#AttachmentAddTicketPrintoutButton').closest('form');
+         $('#AttachmentAddTicketPrintout').val(1);
+         Core.Form.Validate.DisableValidation($Form);
+         $Form.trigger('submit');
+     });
+//]]></script>
+[% END %]
+                        </li>
+[% RenderBlockEnd("AttachmentAddTicketPrintoutButton") %]
                     </ul>
 [% WRAPPER JSOnDocumentComplete %]
 <script type="text/javascript">//<![CDATA[

commit f1b0711d795df8d55bcd9124fa1eb2217438d0be

Fixed time handling routines copied from OTRS 6
Fixed time handling in simple ticket printout attaching mod
introduced in e4b1555ba9515d0a46a860564c70c50edb3b1230 (was
copies from OTRS 6 code by mistake).

Fixes: e4b1555ba9515d0a46a860564c70c50edb3b1230
Related: https://dev.ib.pl/ib/otrs/issues/3
Related: https://github.com/OTRS/otrs/pull/266
Author-Change-Id: IB#1027611

Kernel/Modules/AgentTicketCompose.pm
@@ -523,14 +523,26 @@ sub Run {
                 %Error = ();
                 $Error{AttachmentAddTicketPrintout} = 1;
 
-                # assemble file name
-                my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
-                if ( $Self->{UserTimeZone} ) {
-                    $DateTimeObject->ToTimeZone( TimeZone => $Self->{UserTimeZone} );
+                # get time object and use the UserTimeObject, if the system use UTC as
+                # system time and the TimeZoneUser feature is active
+                if (
+                    !$Kernel::OM->Get('Kernel::System::Time')->ServerLocalTimeOffsetSeconds()
+                    && $Kernel::OM->Get('Kernel::Config')->Get('TimeZoneUser')
+                    && $Self->{UserTimeZone}
+                    )
+                {
+                    $TimeObject = $LayoutObject->{UserTimeObject};
                 }
-                my $Filename = 'Ticket_' . $Tn . '_';
-                $Filename .= $DateTimeObject->Format( Format => '%Y-%m-%d_%H:%M' );
-                $Filename .= '.pdf';
+
+                # return the pdf document
+                my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+                my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
+                    SystemTime => $TimeObject->SystemTime(),
+                );
+                $M = sprintf( "%02d", $M );
+                $D = sprintf( "%02d", $D );
+                $h = sprintf( "%02d", $h );
+                $m = sprintf( "%02d", $m );
 
                 # generate pdf printout
                 my $PDFString = $Kernel::OM->Get('Kernel::Output::PDF::Ticket')->GenerateAgentPDF(
@@ -542,7 +554,7 @@ sub Run {
                     $UploadCacheObject->FormIDAddFile(
                         FormID => $GetParam{FormID},
                         Disposition => 'attachment',
-                        Filename    => $Filename,
+                        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
                         Content     => $PDFString,
                         ContentType => 'application/pdf',
                     );

Kernel/Modules/AgentTicketForward.pm
@@ -902,14 +902,26 @@ sub SendEmail {
             %Error = ();
             $Error{AttachmentAddTicketPrintout} = 1;
 
-            # assemble file name
-            my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
-            if ( $Self->{UserTimeZone} ) {
-                $DateTimeObject->ToTimeZone( TimeZone => $Self->{UserTimeZone} );
+            # get time object and use the UserTimeObject, if the system use UTC as
+            # system time and the TimeZoneUser feature is active
+            if (
+                !$Kernel::OM->Get('Kernel::System::Time')->ServerLocalTimeOffsetSeconds()
+                && $Kernel::OM->Get('Kernel::Config')->Get('TimeZoneUser')
+                && $Self->{UserTimeZone}
+                )
+            {
+                $TimeObject = $LayoutObject->{UserTimeObject};
             }
-            my $Filename = 'Ticket_' . $Tn . '_';
-            $Filename .= $DateTimeObject->Format( Format => '%Y-%m-%d_%H:%M' );
-            $Filename .= '.pdf';
+
+            # return the pdf document
+            my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+            my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
+                SystemTime => $TimeObject->SystemTime(),
+            );
+            $M = sprintf( "%02d", $M );
+            $D = sprintf( "%02d", $D );
+            $h = sprintf( "%02d", $h );
+            $m = sprintf( "%02d", $m );
 
             # generate pdf printout
             my $PDFString = $Kernel::OM->Get('Kernel::Output::PDF::Ticket')->GenerateAgentPDF(
@@ -921,7 +933,7 @@ sub SendEmail {
                 $UploadCacheObject->FormIDAddFile(
                     FormID => $GetParam{FormID},
                     Disposition => 'attachment',
-                    Filename    => $Filename,
+                    Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
                     Content     => $PDFString,
                     ContentType => 'application/pdf',
                 );