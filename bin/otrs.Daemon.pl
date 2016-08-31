deskapp/otrs-ib

commit 4f27a87321e448effc4bb088a3ae4edce5e5c156

Daemon::Log::RotationType option added
OTRS Daemon uses own log rotation method which may create lot of files
in log directory (i.e. if DB is down for a while). This may be hard
to maintain, especially if system uses standard rotation stuff like
logrotate in Debian.

This mod introduces new SysConfig option Daemon::Log::RotationType
which allowes one to set OTRS Daemon log rotation method:
* OTRS - logs rotated by OTRS (default),
* External - OTRS will not rotate logs and external rotation
  mechanism (i.e. logrotate) should be used instead.

Sample logrotate configuration for OTRS Daemon
(/etc/logrotate.d/otrs) that may be used in Debian with
Daemon::Log::RotationType=External:

    /opt/otrs/var/log/Daemon/*.log {
        missingok
        notifempty
        su otrs www-data
        create 660
        postrotate
            if /bin/su -c '/opt/otrs/bin/otrs.Daemon.pl status' -s /bin/bash otrs > /dev/null ; then \
                /bin/su -c '/opt/otrs/bin/otrs.Daemon.pl stop' -s /bin/bash otrs > /dev/null ; \
                /bin/su -c '/opt/otrs/bin/otrs.Daemon.pl start' -s /bin/bash otrs > /dev/null ; \
            fi;
        endscript
    }

Related: https://dev.ib.pl/ib/otrs/issues/39
Author-Change-Id: IB#1050869

bin/otrs.Daemon.pl
@@ -546,23 +546,27 @@ sub _PIDUnlock {
 sub _LogFilesSet {
     my %Param = @_;
 
+    # get config object
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # define log file names
     my $FileStdOut = "$LogDir/$Param{Module}OUT";
     my $FileStdErr = "$LogDir/$Param{Module}ERR";
 
     my $SystemTime = $Kernel::OM->Get('Kernel::System::Time')->SystemTime();
 
-    # backup old log files
-    use File::Copy qw(move);
-    if ( -e "$FileStdOut.log" ) {
-        move( "$FileStdOut.log", "$FileStdOut-$SystemTime.log" );
+    # get log rotation type and backup old logs if logs should be rotated by OTRS
+    my $RotationType = $ConfigObject->Get('Daemon::Log::RotationType') || 'otrs';
+    $RotationType = lc $RotationType;
+    if ( $RotationType eq 'otrs' ) {
+        use File::Copy qw(move);
+        if ( -e "$FileStdOut.log" ) {
+            move( "$FileStdOut.log", "$FileStdOut-$SystemTime.log" );
+        }
+        if ( -e "$FileStdErr.log" ) {
+            move( "$FileStdErr.log", "$FileStdErr-$SystemTime.log" );
+        }
     }
-    if ( -e "$FileStdErr.log" ) {
-        move( "$FileStdErr.log", "$FileStdErr-$SystemTime.log" );
-    }
-
-    # get config object
-    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 
     my $RedirectSTDOUT = $ConfigObject->Get('Daemon::Log::STDOUT') || 0;
     my $RedirectSTDERR = $ConfigObject->Get('Daemon::Log::STDERR') || 0;
@@ -575,29 +579,31 @@ sub _LogFilesSet {
         open STDERR, '>>', "$FileStdErr.log";
     }
 
-    # remove not needed log files
-    my $DaysToKeep = $ConfigObject->Get('Daemon::Log::DaysToKeep') || 1;
-    my $DaysToKeepTime = $SystemTime - $DaysToKeep * 24 * 60 * 60;
+    # remove not needed log files if OTRS rotation is enabled
+    if ( $RotationType eq 'otrs' ) {
+        my $DaysToKeep = $ConfigObject->Get('Daemon::Log::DaysToKeep') || 1;
+        my $DaysToKeepTime = $SystemTime - $DaysToKeep * 24 * 60 * 60;
 
-    my @LogFiles = glob "$LogDir/*.log";
+        my @LogFiles = glob "$LogDir/*.log";
 
-    LOGFILE:
-    for my $LogFile (@LogFiles) {
+        LOGFILE:
+        for my $LogFile (@LogFiles) {
 
-        # skip if is not a backup file
-        next LOGFILE if ( $LogFile !~ m{(?: .* /)* $Param{Module} (?: OUT|ERR ) - (\d+) \.log}igmx );
+            # skip if is not a backup file
+            next LOGFILE if ( $LogFile !~ m{(?: .* /)* $Param{Module} (?: OUT|ERR ) - (\d+) \.log}igmx );
 
-        # do not delete files during keep period if they have content
-        next LOGFILE if ( ( $1 > $DaysToKeepTime ) && -s $LogFile );
+            # do not delete files during keep period if they have content
+            next LOGFILE if ( ( $1 > $DaysToKeepTime ) && -s $LogFile );
 
-        # delete file
-        if ( !unlink $LogFile ) {
+            # delete file
+            if ( !unlink $LogFile ) {
 
-            # log old backup file cannot be deleted
-            $Kernel::OM->Get('Kernel::System::Log')->Log(
-                Priority => 'error',
-                Message  => "Daemon: $Param{Module} could not delete old log file $LogFile! $!",
-            );
+                # log old backup file cannot be deleted
+                $Kernel::OM->Get('Kernel::System::Log')->Log(
+                    Priority => 'error',
+                    Message  => "Daemon: $Param{Module} could not delete old log file $LogFile! $!",
+                );
+            }
         }
     }
 
@@ -606,6 +612,11 @@ sub _LogFilesSet {
 
 sub _LogFilesCleanup {
     my %Param = @_;
+
+    # skip cleanup if OTRS log rotation is not enabled
+    my $RotationType = $Kernel::OM->Get('Kernel::Config')->Get('Daemon::Log::RotationType') || 'otrs';
+    $RotationType = lc $RotationType;
+    return 1 if $RotationType ne 'otrs';
 
     my @LogFiles = glob "$LogDir/*.log";
 

Kernel/Config/Files/Daemon.xml
@@ -43,7 +43,17 @@
             </Array>
         </Setting>
     </ConfigItem>
-
+    <ConfigItem Name="Daemon::Log::RotationType" Required="1" Valid="1">
+        <Description Translatable="1">Type of daemon log rotation to use: OTRS = logs rotated by OTRS; External = OTRS will not rotate logs and external rotation mechanism (i.e. logrotate) should be used instead.</Description>
+        <Group>Daemon</Group>
+        <SubGroup>Core::Log</SubGroup>
+        <Setting>
+            <Option SelectedID="otrs"><!-- default OTRS -->
+                <Item Key="otrs" Translatable="1">OTRS</Item>
+                <Item Key="external" Translatable="1">External</Item>
+            </Option>
+        </Setting>
+    </ConfigItem>
     <ConfigItem Name="Daemon::Log::DaysToKeep" Required="0" Valid="0">
         <Description Translatable="1">Defines the number of days to keep the daemon log files.</Description>
         <Group>Daemon</Group>

commit d68fac6c00de4d2fd72bbabcd3a9b505b6fbd15c

Daemon::Log::RotationType code simplified
Daemon::Log::RotationType code simplified according to
upstream remarks https://github.com/OTRS/otrs/pull/1124

Fixes: 4f27a87321e448effc4bb088a3ae4edce5e5c156
Related: https://dev.ib.pl/ib/otrs/issues/39
Related: https://github.com/OTRS/otrs/pull/1124
Author-Change-Id: IB#1050869

bin/otrs.Daemon.pl
@@ -556,8 +556,7 @@ sub _LogFilesSet {
     my $SystemTime = $Kernel::OM->Get('Kernel::System::Time')->SystemTime();
 
     # get log rotation type and backup old logs if logs should be rotated by OTRS
-    my $RotationType = $ConfigObject->Get('Daemon::Log::RotationType') || 'otrs';
-    $RotationType = lc $RotationType;
+    my $RotationType = lc $ConfigObject->Get('Daemon::Log::RotationType') || 'otrs';
     if ( $RotationType eq 'otrs' ) {
         use File::Copy qw(move);
         if ( -e "$FileStdOut.log" ) {
@@ -614,8 +613,7 @@ sub _LogFilesCleanup {
     my %Param = @_;
 
     # skip cleanup if OTRS log rotation is not enabled
-    my $RotationType = $Kernel::OM->Get('Kernel::Config')->Get('Daemon::Log::RotationType') || 'otrs';
-    $RotationType = lc $RotationType;
+    my $RotationType = lc $Kernel::OM->Get('Kernel::Config')->Get('Daemon::Log::RotationType') || 'otrs';
     return 1 if $RotationType ne 'otrs';
 
     my @LogFiles = glob "$LogDir/*.log";