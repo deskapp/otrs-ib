deskapp/otrs-ib

commit cf34538ac9bad84f4fd3ad4b23f21b28d99cf7e0

Errors on entering AdminCustomerUser with RunInitialWildcardSearch disabled
When AdminCustomerUser::RunInitialWildcardSearch is disabled (i.e. for
performance reasons) and agent opens AdminCustomerUser (which opens with
empty list) errors like

    Message: Need Search, UserLogin, PostMasterSearch, CustomerIDRaw or CustomerID!

occur in apache error.log (for DB and similar one for LDAP backend if present).

This mod resolves issue as above.

Related: https://dev.ib.pl/ib/otrs/issues/88
Author-Change-Id: IB#1055644

Kernel/Modules/AdminCustomerCompany.pm
@@ -520,43 +520,44 @@ sub _Overview {
         );
     }
 
-    # get config object
-    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
-
-    # same Limit as $Self->{CustomerCompanyMap}->{'CustomerCompanySearchListLimit'}
-    # smallest Limit from all sources
-    my $Limit = 400;
-    SOURCE:
-    for my $Count ( '', 1 .. 10 ) {
-        next SOURCE if !$ConfigObject->Get("CustomerCompany$Count");
-        my $CustomerUserMap = $ConfigObject->Get("CustomerCompany$Count");
-        next SOURCE if !$CustomerUserMap->{CustomerCompanySearchListLimit};
-        if ( $CustomerUserMap->{CustomerCompanySearchListLimit} < $Limit ) {
-            $Limit = $CustomerUserMap->{CustomerCompanySearchListLimit};
-        }
-    }
-
-    my %ListAll = $CustomerCompanyObject->CustomerCompanyList(
-        Search => $Param{Search},
-        Limit  => $Limit + 1,
-        Valid  => 0,
-    );
-
-    if ( keys %ListAll <= $Limit ) {
-        my $ListAll = keys %ListAll;
-        $LayoutObject->Block(
-            Name => 'OverviewHeader',
-            Data => {
-                ListAll => $ListAll,
-                Limit   => $Limit,
-            },
-        );
-    }
-
-    my %List = ();
-
     # if there are any registries to search, the table is filled and shown
     if ( $Param{Search} ) {
+
+        # get config object
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+        my %List = ();
+
+        # same Limit as $Self->{CustomerCompanyMap}->{'CustomerCompanySearchListLimit'}
+        # smallest Limit from all sources
+        my $Limit = 400;
+        SOURCE:
+        for my $Count ( '', 1 .. 10 ) {
+            next SOURCE if !$ConfigObject->Get("CustomerCompany$Count");
+            my $CustomerUserMap = $ConfigObject->Get("CustomerCompany$Count");
+            next SOURCE if !$CustomerUserMap->{CustomerCompanySearchListLimit};
+            if ( $CustomerUserMap->{CustomerCompanySearchListLimit} < $Limit ) {
+                $Limit = $CustomerUserMap->{CustomerCompanySearchListLimit};
+            }
+        }
+
+        my %ListAll = $CustomerCompanyObject->CustomerCompanyList(
+            Search => $Param{Search},
+            Limit  => $Limit + 1,
+            Valid  => 0,
+        );
+
+        if ( keys %ListAll <= $Limit ) {
+            my $ListAll = keys %ListAll;
+            $LayoutObject->Block(
+                Name => 'OverviewHeader',
+                Data => {
+                    ListAll => $ListAll,
+                    Limit   => $Limit,
+                },
+            );
+        }
+
         my %List = $CustomerCompanyObject->CustomerCompanyList(
             Search => $Param{Search},
             Valid  => 0,

Kernel/Modules/AdminCustomerUser.pm
@@ -667,43 +667,43 @@ sub _Overview {
         );
     }
 
-    # get config object
-    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
-
-    # same Limit as $Self->{CustomerUserMap}->{CustomerUserSearchListLimit}
-    # smallest Limit from all sources
-    my $Limit = 400;
-    SOURCE:
-    for my $Count ( '', 1 .. 10 ) {
-        next SOURCE if !$ConfigObject->Get("CustomerUser$Count");
-        my $CustomerUserMap = $ConfigObject->Get("CustomerUser$Count");
-        next SOURCE if !$CustomerUserMap->{CustomerUserSearchListLimit};
-        if ( $CustomerUserMap->{CustomerUserSearchListLimit} < $Limit ) {
-            $Limit = $CustomerUserMap->{CustomerUserSearchListLimit};
-        }
-    }
-
-    my %AllUsers = $CustomerUserObject->CustomerSearch(
-        Search => $Param{Search},
-        Limit  => $Limit + 1,
-        Valid  => 0,
-    );
-
-    if ( keys %AllUsers <= $Limit ) {
-        my $ListAll = keys %AllUsers;
-        $LayoutObject->Block(
-            Name => 'OverviewHeader',
-            Data => {
-                ListAll => $ListAll,
-                Limit   => $Limit,
-            },
-        );
-    }
-
-    # when there is no data to show, a message is displayed on the table with this colspan
-    my $ColSpan = 6;
-
     if ( $Param{Search} ) {
+
+        # get config object
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+        # when there is no data to show, a message is displayed on the table with this colspan
+        my $ColSpan = 6;
+
+        # same Limit as $Self->{CustomerUserMap}->{CustomerUserSearchListLimit}
+        # smallest Limit from all sources
+        my $Limit = 400;
+        SOURCE:
+        for my $Count ( '', 1 .. 10 ) {
+            next SOURCE if !$ConfigObject->Get("CustomerUser$Count");
+            my $CustomerUserMap = $ConfigObject->Get("CustomerUser$Count");
+            next SOURCE if !$CustomerUserMap->{CustomerUserSearchListLimit};
+            if ( $CustomerUserMap->{CustomerUserSearchListLimit} < $Limit ) {
+                $Limit = $CustomerUserMap->{CustomerUserSearchListLimit};
+            }
+        }
+
+        my %AllUsers = $CustomerUserObject->CustomerSearch(
+            Search => $Param{Search},
+            Limit  => $Limit + 1,
+            Valid  => 0,
+        );
+
+        if ( keys %AllUsers <= $Limit ) {
+            my $ListAll = keys %AllUsers;
+            $LayoutObject->Block(
+                Name => 'OverviewHeader',
+                Data => {
+                    ListAll => $ListAll,
+                    Limit   => $Limit,
+                },
+            );
+        }
 
         my %List = $CustomerUserObject->CustomerSearch(
             Search => $Param{Search},

commit 3ef63a429e10836345b20d96dbd5043eea1226dc

Removed uneccessarry variable declaration
Removed uneccessarry variable declaration.

Fixes: cf34538ac9bad84f4fd3ad4b23f21b28d99cf7e0
Related: https://dev.ib.pl/ib/otrs/issues/88
Author-Change-Id: IB#1055644

Kernel/Modules/AdminCustomerCompany.pm
@@ -526,8 +526,6 @@ sub _Overview {
         # get config object
         my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 
-        my %List = ();
-
         # same Limit as $Self->{CustomerCompanyMap}->{'CustomerCompanySearchListLimit'}
         # smallest Limit from all sources
         my $Limit = 400;