deskapp/otrs-ib

commit 89f47a0b4d06bf16998040843ed4c685edc7199f

Added Sphinx full text searching module for OTRS (IB#1046583)

Kernel/System/Ticket/ArticleSearchIndex/SphinxAndStaticDB.pm
@@ -1,0 +1,320 @@
+# --
+# Kernel/System/Ticket/ArticleSearchIndex/SphinxAndStaticDB.pm - article search index backend static with Sphinx support
+# Copyright (C) 2014-2015 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
+# Based on StaticDB.pm by OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Ticket::ArticleSearchIndex::SphinxAndStaticDB;
+
+use strict;
+use warnings;
+
+use Kernel::System::Sphinx;
+
+use vars qw(@ISA);
+
+our @ObjectDependencies = (
+    'Kernel::System::Log',
+    'Kernel::System::Main',
+);
+
+sub ArticleIndexBackendInit {
+    my ( $Self, %Param ) = @_;
+
+    if ( !$Kernel::OM->Get('Kernel::System::Main')->Require('Kernel::System::Ticket::ArticleSearchIndex::StaticDB') ) {
+        die "Can't load ticket search index backend module Kernel::System::Ticket::ArticleSearchIndex::StaticDB! $@";
+    }
+    push @ISA, 'Kernel::System::Ticket::ArticleSearchIndex::StaticDB';
+
+    $Self->{SphinxObject} = Kernel::System::Sphinx->new(%Param);
+
+    # save sphinx update only mode status
+    $Self->{UpdateOnly} = 1;
+    if ($Self->{SphinxObject}) {
+        $Self->{UpdateOnly} = $Self->{SphinxObject}->UpdateOnly();
+    }
+
+    return $Self->SUPER::ArticleIndexBackendInit(%Param);
+}
+
+sub ArticleIndexBuild {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(ArticleID UserID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => "Need $Needed!" );
+            return;
+        }
+    }
+
+    # skip rebuilding parent index if requested; for faster
+    # Sphinx index creation/rebuilding in UpdateOnly mode
+    if (!$Param{MainIndexOnly}) {
+        return if !$Self->SUPER::ArticleIndexBuild(%Param);
+    }
+
+    if ($Self->{SphinxObject}) {
+
+        my %Article = $Self->ArticleGet(
+            ArticleID     => $Param{ArticleID},
+            UserID        => $Param{UserID},
+            DynamicFields => 0,
+        );
+
+        if (%Article) {
+            # sphinxql does not accept NULL values; replace NULLs with empty string
+            for (qw(From To Cc Subject MessageID Body)) {
+                if ( !defined($Article{$_}) ) {
+                    $Article{$_} = '';
+                }
+            }
+
+            # feed content to sphinx
+            my $ArchiveFlag = ($Article{ArchiveFlag} && $Article{ArchiveFlag} eq 'y') ? 1 : 0;
+            $Self->{SphinxObject}->Do(
+                SphinxQL => 'REPLACE INTO index_name (id, ticket_id, queue_id, archive_flag,
+                    a_from, a_to, a_cc, a_subject, a_message_id, a_body)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                Bind => [
+                    \$Article{ArticleID}, \$Article{TicketID},  \$Article{QueueID}, \$ArchiveFlag,
+                    \$Article{From},      \$Article{To},        \$Article{Cc},
+                    \$Article{Subject},   \$Article{MessageID}, \$Article{Body},
+                ],
+            );
+        }
+    }
+
+    return 1;
+}
+
+sub ArticleIndexDelete {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(ArticleID UserID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => "Need $Needed!" );
+            return;
+        }
+    }
+
+    return if !$Self->SUPER::ArticleIndexDelete(%Param);
+
+    if ($Self->{SphinxObject}) {
+        # delete article
+        $Self->{SphinxObject}->Do(
+            SphinxQL  => 'DELETE FROM index_name WHERE id = ' . $Param{ArticleID},
+        );
+    }
+
+    return 1;
+}
+
+sub ArticleIndexDeleteTicket {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(TicketID UserID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => "Need $Needed!" );
+            return;
+        }
+    }
+
+    return if !$Self->SUPER::ArticleIndexDeleteTicket(%Param);
+
+    if ($Self->{SphinxObject}) {
+
+        # we need to delete articles in loop because Sphinx doesn't support attributes
+        # in delete's where condition and may not return all matching articles in one
+        # query
+        my $RecordsFound;
+        do {
+            $RecordsFound = 0;
+
+            # get article ids (limit to 100 articles per one search)
+            my $Result = $Self->{SphinxObject}->Prepare(
+                SphinxQL => 'SELECT id FROM index_name WHERE ticket_id = '
+                    . $Param{TicketID},
+                Limit => 100,
+            );
+
+            if ($Result) {
+                # fetch the result
+                my @ResponseData;
+
+                while ( my @Row = $Self->{SphinxObject}->FetchrowArray() ) {
+                    push( @ResponseData, $Row[0] );
+                    $RecordsFound = 1;
+                }
+
+                # delete articles if found
+                if ($RecordsFound) {
+                    # delete articles
+                    $Self->{SphinxObject}->Do(
+                        SphinxQL  => 'DELETE FROM index_name WHERE id in ('
+                            . join(', ', @ResponseData) . ')' ,
+                    );
+                }
+            }
+        } until ($RecordsFound == 0);
+    }
+
+    return 1;
+}
+
+sub ArticleIndexMergeTicket {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(MainTicketID MergeTicketID UserID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => "Need $Needed!" );
+            return;
+        }
+    }
+
+    return if !$Self->SUPER::ArticleIndexMergeTicket(%Param);
+
+    if ($Self->{SphinxObject}) {
+        # update ticket id in merged articles
+        $Self->{SphinxObject}->Do(
+            SphinxQL  => 'UPDATE index_name SET ticket_id='
+                . $Param{MainTicketID} . ' WHERE ticket_id=' . $Param{MergeTicketID},
+        );
+    }
+
+    return 1;
+}
+
+sub _ArticleIndexQuerySQL {
+    my ( $Self, %Param ) = @_;
+
+    if ( !$Param{Data} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => "Need Data!" );
+        return;
+    }
+
+    # use parent module if sphinx not available OR not fired from toolbar
+    # OR in update only mode
+    if ( (!$Self->{SphinxObject})
+        || (!$Param{Data}->{FulltextToolBarSearch})
+        || $Self->{UpdateOnly} ) {
+        return $Self->SUPER::_ArticleIndexQuerySQL(%Param);
+    }
+
+    # no article_table SQL join required if querying sphinx
+    return ' ';
+}
+
+sub _ArticleIndexQuerySQLExt {
+    my ( $Self, %Param ) = @_;
+
+    if ( !$Param{Data} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => "Need Data!" );
+        return;
+    }
+
+    # use parent module if sphinx not available OR not fired from toolbar
+    # OR in update only mode
+    if ( (!$Self->{SphinxObject})
+        || (!$Param{Data}->{FulltextToolBarSearch})
+        || $Self->{UpdateOnly} ) {
+        return $Self->SUPER::_ArticleIndexQuerySQLExt(%Param);
+    }
+
+    # sphinx searching mode; group by ticket_id in sphinx returns only one document
+    # with distinct ticket_id so we will use it instead of "select distinct(ticket_id)..."
+    # construct which is unavailable in sphinx
+    my $Result = ' AND 1=0 ';
+
+    # build queue access condition for non-admin users
+    my $QueueCondition = '';
+    if ( $Param{Data}->{UserID} && $Param{Data}->{UserID} != 1 ) {
+        # get list of all queues user that has ro access to
+        my %Queues = $Self->{QueueObject}->GetAllQueues(
+            UserID => $Param{Data}->{UserID},
+            Type => 'ro',
+        );
+
+        # return empty result if no permitted queues exist
+        return $Result if !%Queues;
+
+        $QueueCondition = " AND queue_id IN (${\(join ', ' , sort {$a <=> $b} keys %Queues)})";
+    }
+
+    # build archive state condition if defined
+    my $ArchiveCondition = (defined $Param{Data}->{ArchiveFlag})
+        ? ' AND archive_flag = ' . $Param{Data}->{ArchiveFlag} : '';
+
+    # disable some sphinx operators (replace with space in search term)
+    # to avoid sphinx errors in case it appears in search terms
+    # (i.e. e-mail address or path specifications)
+    $Param{Data}->{Fulltext} =~ s/[@\/]/ /g;
+
+    my $PrepareResult = $Self->{SphinxObject}->Prepare(
+        SphinxQL => 'SELECT ticket_id FROM index_name WHERE MATCH(?) '
+            . $QueueCondition . $ArchiveCondition
+            . ' GROUP BY ticket_id ORDER BY ticket_id DESC',
+        Bind => [ \$Param{Data}->{Fulltext} ],
+        Limit => $Param{Data}->{Limit},
+    );
+
+    if ($PrepareResult) {
+        # prepare sphinx based where condition with ticket id found
+        my @ResponseData;
+        my $RecordsFound;
+        while ( my @Row = $Self->{SphinxObject}->FetchrowArray() ) {
+            push( @ResponseData, $Row[0] );
+            $RecordsFound = 1;
+        }
+        if ($RecordsFound) {
+            $Result = ' AND st.id in (' . join(', ', @ResponseData) . ')' ,
+        }
+    }
+
+    return $Result;
+}
+
+# update index attributes (queue_id, achive_flag) aftger ticket changes
+sub ArticleIndexUpdateAttr {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Needed (qw(TicketID UserID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => "Need $Needed!" );
+            return;
+        }
+    }
+
+    return if !$Self->SUPER::ArticleIndexUpdateAttr(%Param);
+
+    if ($Self->{SphinxObject}) {
+
+        my %Ticket = $Self->TicketGet(
+            TicketID      => $Param{TicketID},
+            UserID        => $Param{UserID},
+            DynamicFields => 0,
+        );
+
+        if (%Ticket) {
+            # update search index attributes
+            my $ArchiveFlag = ($Ticket{ArchiveFlag} && $Ticket{ArchiveFlag} eq 'y') ? 1 : 0;
+            $Self->{SphinxObject}->Do(
+                SphinxQL => 'UPDATE index_name SET queue_id = ' . $Ticket{QueueID}
+                    . ', archive_flag = ' . $ArchiveFlag
+                    . ' WHERE ticket_id = ' . $Param{TicketID},
+            );
+        }
+    }
+
+    return 1;
+}
+
+1;

Kernel/Config/Files/Ticket.xml
@@ -198,6 +198,17 @@
             </Option>
         </Setting>
     </ConfigItem>
+    <ConfigItem Name="Ticket::ArchiveSystem::ToolBarSearchFulltextAllTickets" Required="1" Valid="1">
+        <Description Translatable="1">Enables searching all (archived and unarchived) tickets in toolbar fulltext search (by default only unarchived ticket are searched).</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Core::Ticket</SubGroup>
+        <Setting>
+            <Option SelectedID="0">
+                <Item Key="0">No</Item>
+                <Item Key="1">Yes</Item>
+            </Option>
+        </Setting>
+    </ConfigItem>
     <ConfigItem Name="Ticket::CustomerArchiveSystem" Required="1" Valid="1">
         <Description Translatable="1">Activates the ticket archive system search in the customer interface.</Description>
         <Group>Ticket</Group>
@@ -507,6 +518,21 @@
                 <Item Key="WordCountMax">1000</Item>
                 <Item Key="WordLengthMin">3</Item>
                 <Item Key="WordLengthMax">30</Item>
+            </Hash>
+        </Setting>
+    </ConfigItem>
+    <ConfigItem Name="Ticket::SearchIndex::Sphinx" Required="0" Valid="0" ConfigLevel="200">
+        <Description Translatable="1">Sphinx search settings.</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Core::FulltextSearch</SubGroup>
+        <Setting>
+            <Hash>
+                <Item Key="host">127.0.0.1</Item>
+                <Item Key="index_name">article_search</Item>
+                <Item Key="max_matches">2000</Item>
+                <Item Key="port">9306</Item>
+                <Item Key="ranker">none</Item>
+                <Item Key="update_only">1</Item>
             </Hash>
         </Setting>
     </ConfigItem>
@@ -1326,6 +1352,17 @@
             <Hash>
                 <Item Key="Module">Kernel::System::Ticket::Event::ArticleSearchIndex</Item>
                 <Item Key="Event">(ArticleCreate|ArticleUpdate)</Item>
+            </Hash>
+        </Setting>
+    </ConfigItem>
+    <ConfigItem Name="Ticket::EventModulePost###99-ArticleSearchIndexUpdateAttr" Required="0" Valid="0">
+        <Description Translatable="1">Updates an article index attributes right after the ticket update.</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Core::FulltextSearch</SubGroup>
+        <Setting>
+            <Hash>
+                <Item Key="Module">Kernel::System::Ticket::Event::ArticleSearchIndexUpdateAttr</Item>
+                <Item Key="Event">(TicketQueueUpdate|TicketArchiveFlagUpdate)</Item>
             </Hash>
         </Setting>
     </ConfigItem>

Kernel/Modules/AgentTicketSearch.pm
@@ -1,5 +1,6 @@
 # --
 # Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2014-2015 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -316,6 +317,17 @@ sub Run {
             $Self->{Profile} = 'last-search';
         }
 
+        # check and remember if it's fulltext search from toolbar
+        if (
+            $GetParam{Fulltext}
+            && $ParamObject->GetParam( Param => 'CheckTicketNumberAndRedirect' )
+            && $GetParam{ResultForm} ne 'Normal'
+            && $GetParam{ResultForm} ne 'Print'
+            )
+        {
+            $GetParam{FulltextToolBarSearch} = 1;
+        }
+
         # save search profile (under last-search or real profile name)
         $Self->{SaveProfile} = 1;
 
@@ -487,7 +499,15 @@ sub Run {
                 $GetParam{ArchiveFlags} = ['y'];
             }
             else {
-                $GetParam{ArchiveFlags} = ['n'];
+                # by default search only unarchived tickets; search all
+                # if configured for toolbar fulltext searches
+                if ($GetParam{FulltextToolBarSearch}
+                        && $ConfigObject->Get('Ticket::ArchiveSystem::ToolBarSearchFulltextAllTickets')) {
+                    $GetParam{ArchiveFlags} = [ 'y', 'n' ];
+                }
+                else {
+                    $GetParam{ArchiveFlags} = ['n'];
+                }
             }
         }
 

Kernel/System/Console/Command/Maint/Ticket/FulltextIndexRebuild.pm
@@ -1,5 +1,6 @@
 # --
 # Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2015 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -31,7 +32,18 @@ sub Configure {
         HasValue    => 1,
         ValueRegex  => qr/^\d+$/smx,
     );
-
+    $Self->AddOption(
+        Name        => 'all-tickets',
+        Description => 'Rebuild index of all tickets (only unarchived tickets by default).',
+        Required    => 0,
+        HasValue    => 0,
+    );
+    $Self->AddOption(
+        Name        => 'main-index-only',
+        Description => 'Rebuild only main index without any parent indexes.',
+        Required    => 0,
+        HasValue    => 0,
+    );
     return;
 }
 
@@ -45,9 +57,11 @@ sub Run {
 
     my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
 
-    # get all tickets
+    # get all unarchived tickets (or all tickets if --all-tickets option given)
+    my $ArchiveFlags = ( $Self->GetOption('all-tickets') ) ? ['y', 'n'] : ['n'];
+
     my @TicketIDs = $TicketObject->TicketSearch(
-        ArchiveFlags => [ 'y', 'n' ],
+        ArchiveFlags => $ArchiveFlags,
         OrderBy      => 'Down',
         SortBy       => 'Age',
         Result       => 'ARRAY',
@@ -72,8 +86,9 @@ sub Run {
 
         for my $ArticleID (@ArticleIndex) {
             $TicketObject->ArticleIndexBuild(
-                ArticleID => $ArticleID,
-                UserID    => 1,
+                ArticleID     => $ArticleID,
+                UserID        => 1,
+                MainIndexOnly => $Self->GetOption('main-index-only'),
             );
         }
 

Kernel/System/Sphinx.pm
@@ -1,0 +1,384 @@
+# --
+# Kernel/System/Sphinx.pm - Sphinx search engine routines
+# Copyright (C) 2014-2015 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
+# Based on DB.pm by OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Sphinx;
+
+use strict;
+use warnings;
+
+use DBI;
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::Encode',
+    'Kernel::System::Log',
+    'Kernel::System::Time',
+);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # 0=off; 1=updates; 2=+selects; 3=+Connects;
+    $Self->{Debug} = $Param{Debug} || 0;
+
+    # get sphinx parameters
+    $Self->{Config} = $Kernel::OM->Get('Kernel::Config')->Get('Ticket::SearchIndex::Sphinx');
+    if ( !$Self->{Config} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => "Sphinx enabled but no Ticket::SearchIndex::Sphinx parameters defined!",
+        );
+        return;
+    }
+
+    $Self->{SphinxHost} = $Self->{Config}->{host} || '127.0.0.1';
+    $Self->{SphinxPort} = $Self->{Config}->{port} || 9306;
+    $Self->{SphinxMaxMatches} = $Self->{Config}->{max_matches} || 1000;
+    $Self->{SphinxRanker} = $Self->{Config}->{ranker} || 'none';
+    $Self->{SphinxIndexName} = $Self->{Config}->{index_name} || 'article_search';
+    $Self->{SphinxUpdateOnly} = (defined $Self->{Config}->{update_only}) ? $Self->{Config}->{update_only} : 1;
+
+    # prepare Sphinx connection string (using Sphinx MySQL protocol interface);
+    # persistent connections to Sphinx available using DBI
+    my $DSN = 'DBI:mysql:host=' . $Self->{SphinxHost} . ';port=' . $Self->{SphinxPort};
+
+    # DBI/DBD::mysql attributes
+    # disable automatic reconnects as they do not execute "SET NAMES utf8", which will
+    # cause charset problems
+    my $Attribute = {
+        mysql_auto_reconnect => 0,
+    };
+
+    # sphinx connect
+    $Self->{sph} = DBI->connect(
+        $DSN,
+        '',
+        '',
+        $Attribute,
+    );
+
+    if ( !$Self->{sph} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Caller   => 1,
+            Priority => 'Error',
+            Message  => $DBI::errstr,
+        );
+        return;
+    }
+
+    $Self->Do( SphinxQL => 'SET NAMES utf8' );
+
+    return $Self;
+}
+
+=item Do()
+
+to insert, update or delete values
+
+    $SphinxObject->Do( SphinxQL => "SELECT * FROM article_search WHERE MATCH('BOD*')" );
+
+    you also can use DBI bind values (used for large strings):
+
+    my $Var1 = 'dog1';
+    my $Var2 = 'dog2';
+
+    $SphinxObject->Do(
+        SphinxQL  => "INSERT INTO index (name1, name2) VALUES (?, ?)",
+        Bind => [ \$Var1, \$Var2 ],
+    );
+
+=cut
+
+sub Do {
+    my ( $Self, %Param ) = @_;
+
+    # cancel if no sphinx connection available
+    return if !$Self->{sph};
+
+    # check needed stuff
+    if ( !$Param{SphinxQL} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => 'Need SphinxQL!' );
+        return;
+    }
+
+    # check bind params
+    my @Array;
+    if ( $Param{Bind} ) {
+        for my $Data ( @{ $Param{Bind} } ) {
+            if ( ref $Data eq 'SCALAR' ) {
+                push @Array, $$Data;
+            }
+            else {
+                $Kernel::OM->Get('Kernel::System::Log')->Log(
+                    Caller   => 1,
+                    Priority => 'Error',
+                    Message  => 'No SCALAR param in Bind!',
+                );
+                return;
+            }
+        }
+    }
+
+    # Replace current_timestamp with real time stamp.
+    # - This avoids time inconsistencies of app and db server
+    my $Timestamp = $Kernel::OM->Get('Kernel::System::Time')->CurrentTimestamp();
+    $Param{SphinxQL} =~ s{
+        (?<= \s | \( | , )  # lookahead
+        current_timestamp   # replace current_timestamp by 'yyyy-mm-dd hh:mm:ss'
+        (?=  \s | \) | , )  # lookbehind
+    }
+    {
+        '$Timestamp'
+    }xmsg;
+
+    # Replace index_name with real index name from config.
+    $Param{SphinxQL} =~ s{
+        (?<= \s | \( | , )  # lookahead
+        index_name
+        (?=  \s | \) | , )  # lookbehind
+    }
+    { $Self->{SphinxIndexName} }xmsg;
+
+    # debug
+    if ( $Self->{Debug} > 0 ) {
+        $Self->{DoCounter}++;
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Caller   => 1,
+            Priority => 'debug',
+            Message  => "Sphinx.pm->Do SphinxQL: '$Param{SphinxQL}'",
+        );
+    }
+
+    # check length, don't use more than 4 k
+    if ( bytes::length( $Param{SphinxQL} ) > 4 * 1024 ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Caller   => 1,
+            Priority => 'notice',
+            Message  => 'Your SphinxQL is longer than 4k. Use bind instead! SphinxQL: '
+                . $Param{SphinxQL},
+        );
+    }
+
+    # send SphinxQL to sphinx
+    if ( !$Self->{sph}->do( $Param{SphinxQL}, undef, @Array ) ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Caller   => 1,
+            Priority => 'error',
+            Message  => "$DBI::errstr, SphinxQL: '$Param{SphinxQL}'",
+        );
+        return;
+    }
+
+    return 1;
+}
+
+=item Prepare()
+
+to prepare a SELECT statement
+
+    $SphinxObject->Prepare(
+        SphinxQL   => "SELECT id, name FROM table",
+        Limit => 10,
+    );
+
+or in case you want just to get row 10 until 30
+
+    $SphinxObject->Prepare(
+        SphinxQL   => "SELECT id, name FROM table",
+        Start => 10,
+        Limit => 20,
+    );
+
+in case you don't want utf-8 encoding for some columns, use this:
+
+    $SphinxObject->Prepare(
+        SphinxQL    => "SELECT id, name, content FROM table",
+        Encode => [ 1, 1, 0 ],
+    );
+
+you also can use DBI bind values, required for large strings:
+
+    my $Var1 = 'dog1';
+    my $Var2 = 'dog2';
+
+    $SphinxObject->Prepare(
+        SphinxQL    => "SELECT id, name, content FROM table WHERE name_a = ? AND name_b = ?",
+        Encode => [ 1, 1, 0 ],
+        Bind   => [ \$Var1, \$Var2 ],
+    );
+
+=cut
+
+sub Prepare {
+    my ( $Self, %Param ) = @_;
+
+    # cancel if no sphinx connection available
+    return if !$Self->{sph};
+
+    # check needed stuff
+    if ( !$Param{SphinxQL} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log( Priority => 'error', Message => 'Need SQL!' );
+        return;
+    }
+
+    my $SphinxQL = $Param{SphinxQL};
+    my $Limit    = $Param{Limit} || '';
+    my $Start    = $Param{Start} || '';
+
+    # query limit must not be greater than Sphinx max_matches
+    if ($Limit > $Self->{SphinxMaxMatches}) {
+        $Limit = $Self->{SphinxMaxMatches};
+    }
+
+    if ( defined $Param{Encode} ) {
+        $Self->{Encode} = $Param{Encode};
+    }
+    else {
+        $Self->{Encode} = undef;
+    }
+    $Self->{Limit}        = 0;
+    $Self->{LimitStart}   = 0;
+    $Self->{LimitCounter} = 0;
+
+    # build final select query
+    if ($Limit) {
+        if ($Start) {
+            $Limit = $Limit + $Start;
+            $Self->{LimitStart} = $Start;
+        }
+        $SphinxQL .= " LIMIT $Limit";
+    }
+    $SphinxQL .= ' OPTION ranker=' . $Self->{SphinxRanker}
+        . ', max_matches=' . $Self->{SphinxMaxMatches};
+
+    # Replace index_name with real index name from config.
+    $SphinxQL =~ s{
+        (?<= \s | \( | , )  # lookahead
+        index_name
+        (?=  \s | \) | , )  # lookbehind
+    }
+    { $Self->{SphinxIndexName} }xmsg;
+
+    # debug
+    if ( $Self->{Debug} > 1 ) {
+        $Self->{PrepareCounter}++;
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Caller   => 1,
+            Priority => 'debug',
+            Message  => "Sphinx.pm->Prepare ($Self->{PrepareCounter}/" . time() . ") SphinxQL: '$SphinxQL'",
+        );
+    }
+
+    # check bind params
+    my @Array;
+    if ( $Param{Bind} ) {
+        for my $Data ( @{ $Param{Bind} } ) {
+            if ( ref $Data eq 'SCALAR' ) {
+                push @Array, $$Data;
+            }
+            else {
+                $Kernel::OM->Get('Kernel::System::Log')->Log(
+                    Caller   => 1,
+                    Priority => 'Error',
+                    Message  => 'No SCALAR param in Bind!',
+                );
+                return;
+            }
+        }
+    }
+
+    # do
+    if ( !( $Self->{Cursor} = $Self->{sph}->prepare($SphinxQL) ) ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Caller   => 1,
+            Priority => 'Error',
+            Message  => "$DBI::errstr, SphinxQL: '$SphinxQL'",
+        );
+        return;
+    }
+
+    if ( !$Self->{Cursor}->execute(@Array) ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Caller   => 1,
+            Priority => 'Error',
+            Message  => "$DBI::errstr, SphinxQL: '$SphinxQL'",
+        );
+        return;
+    }
+
+    return 1;
+}
+
+=item FetchrowArray()
+
+to process the results of a SELECT statement
+
+    $SphinxObject->Prepare(
+        SphinxQL   => "SELECT id, name FROM table",
+        Limit => 10
+    );
+
+    while (my @Row = $SphinxObject->FetchrowArray()) {
+        print "$Row[0]:$Row[1]\n";
+    }
+
+=cut
+
+sub FetchrowArray {
+    my $Self = shift;
+
+    # cancel if no sphinx connection available
+    return if !$Self->{sph};
+
+    # fetch first not used rows
+    if ( $Self->{LimitStart} ) {
+        for ( 1 .. $Self->{LimitStart} ) {
+            if ( !$Self->{Cursor}->fetchrow_array() ) {
+                $Self->{LimitStart} = 0;
+                return ();
+            }
+            $Self->{LimitCounter}++;
+        }
+        $Self->{LimitStart} = 0;
+    }
+
+    # return
+    my @Row = $Self->{Cursor}->fetchrow_array();
+
+    # e. g. set utf-8 flag
+    my $Counter = 0;
+    ELEMENT:
+    for my $Element (@Row) {
+        if ( !$Element ) {
+            next ELEMENT;
+        }
+
+        if ( !defined $Self->{Encode} || ( $Self->{Encode} && $Self->{Encode}->[$Counter] ) ) {
+            $Kernel::OM->Get('Kernel::System::Encode')->EncodeInput( \$Element );
+        }
+    }
+    continue {
+        $Counter++;
+    }
+
+    return @Row;
+}
+
+# return current update_only setting
+sub UpdateOnly {
+    my $Self = shift;
+    return $Self->{SphinxUpdateOnly};
+}
+
+1;

Kernel/System/Ticket.pm
@@ -1,5 +1,6 @@
 # --
 # Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2015 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -160,6 +161,9 @@ sub new {
 
     # init of article backend
     $Self->ArticleStorageInit();
+
+    # init of article index backend
+    $Self->ArticleIndexBackendInit(%Param);
 
     return $Self;
 }
@@ -5903,12 +5907,8 @@ sub TicketMerge {
         Bind => [ \$Param{MainTicketID}, \$Param{UserID}, \$Param{MergeTicketID} ],
     );
 
-    # bug 9635
-    # do the same with article_search (harmless if not used)
-    return if !$DBObject->Do(
-        SQL  => 'UPDATE article_search SET ticket_id = ? WHERE ticket_id = ?',
-        Bind => [ \$Param{MainTicketID}, \$Param{MergeTicketID} ],
-    );
+    # change ticket id of merge ticket to main ticket in article search index
+    return if !$Self->ArticleIndexMergeTicket(%Param);
 
     # reassign article history
     return if !$DBObject->Do(

commit 3ab056e4ce682860340e060a7a715dbf4d909bac

Fixed searching for strings with ! in Sphinx
Searching for phrases with ! in Sphinx search introduced in
89f47a0b4d06bf16998040843ed4c685edc7199f caused syntax errors like

    index article_search: syntax error, unexpected $end near ''

This mod replaces ! with space before passing phrase to Sphinx.

Fixes: 89f47a0b4d06bf16998040843ed4c685edc7199f
Related: https://dev.ib.pl/ib/otrs/issues/15
Author-Change-Id: IB#1046583

Kernel/System/Ticket/ArticleSearchIndex/SphinxAndStaticDB.pm
@@ -256,7 +256,7 @@ sub _ArticleIndexQuerySQLExt {
     # disable some sphinx operators (replace with space in search term)
     # to avoid sphinx errors in case it appears in search terms
     # (i.e. e-mail address or path specifications)
-    $Param{Data}->{Fulltext} =~ s/[@\/]/ /g;
+    $Param{Data}->{Fulltext} =~ s/[@\/!]/ /g;
 
     my $PrepareResult = $Self->{SphinxObject}->Prepare(
         SphinxQL => 'SELECT ticket_id FROM index_name WHERE MATCH(?) '