8d0807266f69570d0b32adfafbf19a3daf508b43
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2016-10-05 14:08:58 +0200

    Fixed handling hidden files and filter quotation fixes
    If mail with attachment headers like...
    
        Content-Type: text/plain; charset=UTF-8;
         name="../../../uploads/578_jpg_org.txt"
        Content-Transfer-Encoding: base64
        Content-Disposition: attachment;
         filename="../../../uploads/578_jpg_org.txt"
    
    ...arrives to OTRS, attachment file
    
        _.._.._uploads_578_jpg_org.txt
    
    is created in article dir. This file is not present in the ticket
    attachment list. When generic agent tries to remove ticket, this
    file is not removed and error deleting article dir occurs like this:
    
        Can't remove: [...]2016/10/05/4: Directory not empty!
    
    Problem does not occur if mod described in https://dev.ib.pl/ib/otrs/issues/59
    is applied (this removes periods from filename prefixes using secured
    FilenameCleanUp() function).
    
    This mod fixes problem described above and contains minor quotation
    changes in DirectoryRead calls.
    
    Related: https://dev.ib.pl/ib/otrs/issues/96
    Author-Change-Id: IB#1039611

--- Kernel/System/Ticket/ArticleStorageFS.pm
+++ Kernel/System/Ticket/ArticleStorageFS.pm
@@ -238,8 +238,9 @@ sub ArticleDeleteAttachment {
     if ( -e $Path ) {
 
         my @List = $Kernel::OM->Get('Kernel::System::Main')->DirectoryRead(
-            Directory => $Path,
-            Filter    => "*",
+            Directory     => $Path,
+            Filter        => '*',
+            IncludeHidden => 1,
         );
 
         for my $File (@List) {
@@ -634,9 +635,10 @@ sub ArticleAttachmentIndexRaw {
 
     # try fs
     my @List = $MainObject->DirectoryRead(
-        Directory => "$Self->{ArticleDataDir}/$ContentPath/$Param{ArticleID}",
-        Filter    => "*",
-        Silent    => 1,
+        Directory     => "$Self->{ArticleDataDir}/$ContentPath/$Param{ArticleID}",
+        Filter        => '*',
+        IncludeHidden => 1,
+        Silent        => 1,
     );
 
     FILENAME:
@@ -900,9 +902,10 @@ sub ArticleAttachment {
     my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
 
     my @List = $MainObject->DirectoryRead(
-        Directory => "$Self->{ArticleDataDir}/$ContentPath/$Param{ArticleID}",
-        Filter    => "*",
-        Silent    => 1,
+        Directory     => "$Self->{ArticleDataDir}/$ContentPath/$Param{ArticleID}",
+        Filter        => '*',
+        IncludeHidden => 1,
+        Silent        => 1,
     );
 
     if (@List) {
