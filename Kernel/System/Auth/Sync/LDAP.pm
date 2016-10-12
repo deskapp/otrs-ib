deskapp/otrs-ib

commit 49d58b650e4eb31f4c883e2bb333402b4d40690d

Added extended agent properties synchronization from LDAP backend
This mod allows to synchronize extended OTRS agent properties from LDAP
backend (i.e. OpenLDAP or Active Directory). It also fixes problems with
empty UserMobile properties in user_preferences tables that may be
created after new agent creation and handles better undef/empty string
comparisons that may occur between data stored in OTRS DB and data
comming from LDAP.

With this mod you can specify additional User* properties
in AuthSyncModule::LDAP::UserSyncMap mapping (i.e. UserPhone,
UserDepartment, UserJobTitle) in addition to properties supported
by standard OTRS (UserFirstname, UserLastname, UserEmail, UserMobile).
Extended property names must begin with "User"; you can use it
in signature templates for example.

Usage example: the following setting

    $Self->{'AuthSyncModule::LDAP::UserSyncMap'} = {
        # DB -> LDAP
        UserFirstname   => 'givenName',
        UserLastname    => 'sn',
        UserEmail       => 'mail',
        UserMobile      => 'mobile',
        UserPhone       => 'telephoneNumber',
        UserDepartment  => 'department',
        UserJobTitle    => 'title',
    };

will allow you to use extra properties from Active Directory
in signature templates like this:

    Regards,
    <OTRS_AGENT_UserFirstname> <OTRS_AGENT_UserLastname>
    <OTRS_AGENT_UserJobTitle>
    <OTRS_AGENT_UserDepartment>
    M: <OTRS_AGENT_UserMobile>, T: <OTRS_AGENT_UserPhone>

Extended properties from LDAP are stored in OTRS database
in user_preferences table.

Related: https://dev.ib.pl/ib/otrs/issues/47
Author-Change-Id: IB#1014533

Kernel/System/Auth/Sync/LDAP.pm
@@ -270,12 +270,12 @@ sub Sync {
         # add new user
         if ( %SyncUser && !$UserID ) {
             $UserID = $UserObject->UserAdd(
-                UserTitle => 'Mr/Mrs',
                 UserLogin => $Param{User},
                 %SyncUser,
                 UserType     => 'User',
                 ValidID      => 1,
                 ChangeUserID => 1,
+                Extended     => 1,
             );
             if ( !$UserID ) {
                 $Kernel::OM->Get('Kernel::System::Log')->Log(
@@ -336,7 +336,10 @@ sub Sync {
             my $AttributeChange;
             ATTRIBUTE:
             for my $Attribute ( sort keys %SyncUser ) {
-                next ATTRIBUTE if $SyncUser{$Attribute} eq $UserData{$Attribute};
+                # treat undef and empty string as equal
+                my $SyncUserAttribute = $SyncUser{$Attribute} || '';
+                my $UserDataAttribute = $UserData{$Attribute} || '';
+                next ATTRIBUTE if $SyncUserAttribute eq $UserDataAttribute;
                 $AttributeChange = 1;
                 last ATTRIBUTE;
             }
@@ -349,6 +352,7 @@ sub Sync {
                     %SyncUser,
                     UserType     => 'User',
                     ChangeUserID => 1,
+                    Extended     => 1,
                 );
             }
         }

Kernel/System/User.pm
@@ -341,6 +341,7 @@ to add new users
         UserMobile    => '1234567890', # not required
         ValidID       => 1,
         ChangeUserID  => 123,
+        Extended      => 0, # set extended properties also; disabled by default
     );
 
 =cut
@@ -456,6 +457,29 @@ sub UserAdd {
         Value  => $Param{UserMobile} || '',
     );
 
+    # set extended user preferences if enabled
+    if ( $Param{Extended} ) {
+        foreach my $key ( keys %Param ) {
+            # skip standard and tech (not prefixed with "User") properties
+            next if $key !~ '^User.+';
+            next if $key eq 'UserID';
+            next if $key eq 'UserType';
+            next if $key eq 'UserTitle';
+            next if $key eq 'UserFirstname';
+            next if $key eq 'UserLastname';
+            next if $key eq 'UserLogin';
+            next if $key eq 'UserPw';
+            next if $key eq 'UserEmail';
+            next if $key eq 'UserMobile';
+
+            $Self->SetPreferences(
+                UserID => $UserID,
+                Key => $key,
+                Value => $Param{$key}
+            );
+        }
+    }
+
     # delete cache
     $Kernel::OM->Get('Kernel::System::Cache')->CleanUp(
         Type => $Self->{CacheType},
@@ -478,6 +502,7 @@ to update users
         UserMobile    => '1234567890', # not required
         ValidID       => 1,
         ChangeUserID  => 123,
+        Extended      => 0, # update extended properties also; disabled by default
     );
 
 =cut
@@ -563,6 +588,29 @@ sub UserUpdate {
         Key    => 'UserMobile',
         Value  => $Param{UserMobile} || '',
     );
+
+    # update extended user preferences if enabled
+    if ( $Param{Extended} ) {
+        foreach my $key ( keys %Param ) {
+            # skip standard and tech (not prefixed with "User") properties
+            next if $key !~ '^User.+';
+            next if $key eq 'UserID';
+            next if $key eq 'UserType';
+            next if $key eq 'UserTitle';
+            next if $key eq 'UserFirstname';
+            next if $key eq 'UserLastname';
+            next if $key eq 'UserLogin';
+            next if $key eq 'UserPw';
+            next if $key eq 'UserEmail';
+            next if $key eq 'UserMobile';
+
+            $Self->SetPreferences(
+                UserID => $Param{UserID},
+                Key => $key,
+                Value => $Param{$key}
+            );
+        }
+    }
 
     # update search profiles if the UserLogin changed
     if ( lc $OldUserLogin ne lc $Param{UserLogin} ) {
@@ -1183,11 +1231,10 @@ sub SetPreferences {
         NoOutOfOffice => 1,
     );
 
-    # no updated needed
-    return 1
-        if defined $User{ $Param{Key} }
-        && defined $Param{Value}
-        && $User{ $Param{Key} } eq $Param{Value};
+    # no update needed (treat undef and empty string as equal)
+    my $UserValue = $User{ $Param{Key} } || '';
+    my $ParamValue = $Param{Value} || '';
+    return 1 if $UserValue eq $ParamValue;
 
     # get config object
     my $ConfigObject = $Kernel::OM->Get('Kernel::Config');

commit b18864aaa730fbdb056bcbd422a636a7ae92d594

Fixed undef-empty string comparison in user preferences management
This fixes issue with treating 0 and undef/empty string as equal
which caused problem with saving user preferences changes (i.e.
hiding dashboard elements worked only for session lifetime and
was not stored in DB).

Fixes: 49d58b650e4eb31f4c883e2bb333402b4d40690d
Related: https://dev.ib.pl/ib/otrs/issues/47
Author-Change-Id: IB#1014533

Kernel/System/Auth/Sync/LDAP.pm
@@ -337,8 +337,8 @@ sub Sync {
             ATTRIBUTE:
             for my $Attribute ( sort keys %SyncUser ) {
                 # treat undef and empty string as equal
-                my $SyncUserAttribute = $SyncUser{$Attribute} || '';
-                my $UserDataAttribute = $UserData{$Attribute} || '';
+                my $SyncUserAttribute = $SyncUser{$Attribute} // '';
+                my $UserDataAttribute = $UserData{$Attribute} // '';
                 next ATTRIBUTE if $SyncUserAttribute eq $UserDataAttribute;
                 $AttributeChange = 1;
                 last ATTRIBUTE;

Kernel/System/User.pm
@@ -1232,8 +1232,8 @@ sub SetPreferences {
     );
 
     # no update needed (treat undef and empty string as equal)
-    my $UserValue = $User{ $Param{Key} } || '';
-    my $ParamValue = $Param{Value} || '';
+    my $UserValue = $User{ $Param{Key} } // '';
+    my $ParamValue = $Param{Value} // '';
     return 1 if $UserValue eq $ParamValue;
 
     # get config object