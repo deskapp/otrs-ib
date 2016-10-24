8569786ee178f8de77aa3c737d92fff2bbc33330
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2016-10-24 12:28:58 +0200

    Fixed e-mail address validation while sending autoresponses and article creation on send errors
    OTRS does not check if e-mail specified in From/Reply-To message headers
    is valid before using it for sending autoresponse which throws sending
    errors in case of invalid addresses like
    
        test <test@>
    
    present in From or Reply-To.
    
    In case of e-mail (i.e. autoresponse, forwarding, reply) sending error
    (i.e. bad address like above or other SMTP error like too big message)
    ArticleSend() creates article in DB which is misleading because
    no e-mail message was sent in fact. ArticleSend() should not create
    article in DB if no message was successfully sent to allow agent
    to clearly notice the problem.
    
    This mod fixes issues described above.
    
    Related: https://dev.ib.pl/ib/otrs/issues/101
    Author-Change-Id: IB#1023814

--- Kernel/System/CheckItem.pm
+++ Kernel/System/CheckItem.pm
@@ -86,7 +86,9 @@ returns true if check was successful, if it's false, get the error message
 from CheckError()
 
     my $Valid = $CheckItemObject->CheckEmail(
-        Address => 'info@example.com',
+        Address       => 'info@example.com',
+        SkipDNSChecks => 0, # 0 - do DNS checks if enabled in SysConfig with CheckMXRecord (default)
+                            # 1 - skip DNS checks even if enabled in SysConfig with CheckMXRecord
     );
 
 =cut
@@ -101,6 +103,11 @@ sub CheckEmail {
             Message  => 'Need Address!'
         );
         return;
+    }
+
+    # do DNS check by default if not disabled
+    if ( !defined $Param{SkipDNSChecks} || $Param{SkipDNSChecks} ne '1' ) {
+        $Param{SkipDNSChecks} = 0;
     }
 
     # get config object
@@ -132,7 +139,8 @@ sub CheckEmail {
 
     # mx check
     elsif (
-        $ConfigObject->Get('CheckMXRecord')
+        !$Param{SkipDNSChecks}
+        && $ConfigObject->Get('CheckMXRecord')
         && eval { require Net::DNS }    ## no critic
         )
     {
@@ -196,7 +204,7 @@ sub CheckEmail {
             }
         }
     }
-    elsif ( $ConfigObject->Get('CheckMXRecord') ) {
+    elsif ( !$Param{SkipDNSChecks} && $ConfigObject->Get('CheckMXRecord') ) {
 
         $Kernel::OM->Get('Kernel::System::Log')->Log(
             Priority => 'error',
