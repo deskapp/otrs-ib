deskapp/otrs-ib

commit 8569786ee178f8de77aa3c737d92fff2bbc33330

Fixed e-mail address validation while sending autoresponses and article creation on send errors
OTRS does not check if e-mail specified in From/Reply-To message headers
is valid before using it for sending autoresponse which throws sending
errors in case of invalid addresses like

    test <test@>

present in From or Reply-To.

In case of e-mail (i.e. autoresponse, forwarding, reply) sending error
(i.e. bad address like above or other SMTP error like too big message)
ArticleSend() creates article in DB which is misleading because
no e-mail message was sent in fact. ArticleSend() should not create
article in DB if no message was successfully sent to allow agent
to clearly notice the problem.

This mod fixes issues described above.

Related: https://dev.ib.pl/ib/otrs/issues/101
Author-Change-Id: IB#1023814

Kernel/System/Ticket/Article.pm
@@ -2183,6 +2183,7 @@ sub ArticleSend {
     my ( $Self, %Param ) = @_;
 
     my $ToOrig      = $Param{To}          || '';
+    my $CcOrig      = $Param{Cc}          || '';
     my $Loop        = $Param{Loop}        || 0;
     my $HistoryType = $Param{HistoryType} || 'SendAnswer';
 
@@ -2233,16 +2234,10 @@ sub ArticleSend {
         AttachmentsRef => $Param{Attachment},
     );
 
-    # create article
     my $Time      = $Kernel::OM->Get('Kernel::System::Time')->SystemTime();
     my $Random    = rand 999999;
     my $FQDN      = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
     my $MessageID = "<$Time.$Random\@$FQDN>";
-    my $ArticleID = $Self->ArticleCreate(
-        %Param,
-        MessageID => $MessageID,
-    );
-    return if !$ArticleID;
 
     # send mail
     my ( $HeadRef, $BodyRef ) = $Kernel::OM->Get('Kernel::System::Email')->Send(
@@ -2253,7 +2248,22 @@ sub ArticleSend {
     # return if no mail was able to send
     if ( !$HeadRef || !$BodyRef ) {
         $Kernel::OM->Get('Kernel::System::Log')->Log(
-            Message  => "Impossible to send message to: $Param{'To'} .",
+            Message  => "Error sending message to $Param{'To'} (Message-ID: $MessageID).",
+            Priority => 'error',
+        );
+        return;
+    }
+
+    # create article
+    my $ArticleID = $Self->ArticleCreate(
+        %Param,
+        MessageID => $MessageID,
+    );
+
+    # return if article was not created
+    if ( !$ArticleID ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Message  => "Impossible to create article for e-mail sent (Message-ID: $MessageID).",
             Priority => 'error',
         );
         return;
@@ -2265,12 +2275,22 @@ sub ArticleSend {
         Email     => ${$HeadRef} . "\n" . ${$BodyRef},
         UserID    => $Param{UserID}
     );
-    return if !$Plain;
+
+    # return if plain article was not written
+    if ( !$Plain ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Message  => "Impossible to write plain article for e-mail sent (Message-ID: $MessageID).",
+            Priority => 'error',
+        );
+        return;
+    }
 
     # log
     $Kernel::OM->Get('Kernel::System::Log')->Log(
         Priority => 'info',
-        Message  => "Sent email to '$ToOrig' from '$Param{From}'. "
+        Message  => "Sent email to '$ToOrig' "
+            . ( $CcOrig ? "(cc '$CcOrig') " : '' )
+            . "from '$Param{From} (Message-ID: $MessageID)'. "
             . "HistoryType => $HistoryType, Subject => $Param{Subject};",
     );
 
@@ -2482,20 +2502,39 @@ sub SendAutoResponse {
     ADDRESS:
     for my $Address (@Addresses) {
         my $Email = $EmailParser->GetEmailAddress( Email => $Address );
-        if ( !$Email ) {
+
+        my $EmailValid = 0;
+        my $EmailErrType = '';
+        if ( $Email ) {
+
+            # get check item object
+            my $CheckItemObject = $Kernel::OM->Get('Kernel::System::CheckItem');
+
+            $EmailValid = $CheckItemObject->CheckEmail(
+                Address => $Email,
+                SkipDNSChecks => 1,  # don't skip autoresponding in case of temporary DNS problems
+            );
+
+            if ( !$EmailValid ) {
+                $EmailErrType = ' (' . $CheckItemObject->CheckErrorType() . ')';
+            }
+        }
+
+        # check email address
+        if ( !$EmailValid ) {
 
             # add it to ticket history
             $Self->HistoryAdd(
                 TicketID     => $Param{TicketID},
                 CreateUserID => $Param{UserID},
                 HistoryType  => 'Misc',
-                Name         => "Sent no auto response to '$Address' - no valid email address.",
+                Name         => "Sent no auto response to invalid address '$Address'$EmailErrType",
             );
 
             # log
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'notice',
-                Message  => "Sent no auto response to '$Address' because of invalid address.",
+                Message  => "Sent no auto response to invalid address '$Address'$EmailErrType",
             );
             next ADDRESS;
 
@@ -2596,7 +2635,7 @@ sub SendAutoResponse {
         SenderType     => 'system',
         TicketID       => $Param{TicketID},
         HistoryType    => $HistoryType,
-        HistoryComment => "\%\%$AutoReplyAddresses",
+        HistoryComment => "\%\%$AutoReplyAddresses" . ( $Cc ? ( $AutoReplyAddresses ? ', ' : '' ) . "cc '$Cc'" : '' ),
         From           => "$AutoResponse{SenderRealname} <$AutoResponse{SenderAddress}>",
         To             => $AutoReplyAddresses,
         Cc             => $Cc,
@@ -2609,21 +2648,44 @@ sub SendAutoResponse {
         UserID         => $Param{UserID},
     );
 
-    # log
-    $Kernel::OM->Get('Kernel::System::Log')->Log(
-        Priority => 'info',
-        Message  => "Sent auto response ($HistoryType) for Ticket [$Ticket{TicketNumber}]"
-            . " (TicketID=$Param{TicketID}, ArticleID=$ArticleID) to '$AutoReplyAddresses'."
-    );
+    if ( $ArticleID ) {
 
-    # event
-    $Self->EventHandler(
-        Event => 'ArticleAutoResponse',
-        Data  => {
-            TicketID => $Param{TicketID},
-        },
-        UserID => $Param{UserID},
-    );
+        # log
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'info',
+            Message  => "Sent auto response ($HistoryType) for Ticket [$Ticket{TicketNumber}]"
+                . " (TicketID=$Param{TicketID}, ArticleID=$ArticleID) to '$AutoReplyAddresses'"
+                . ( $Cc ? " and cc '$Cc'." : '.' )
+        );
+
+        # event
+        $Self->EventHandler(
+            Event => 'ArticleAutoResponse',
+            Data  => {
+                TicketID => $Param{TicketID},
+            },
+            UserID => $Param{UserID},
+        );
+    }
+    else {
+        my $ErrorMessage = "Error sending auto response ($HistoryType) for Ticket [$Ticket{TicketNumber}]"
+                . " (TicketID=$Param{TicketID}) to '$AutoReplyAddresses'"
+                . ( $Cc ? " and cc '$Cc'." : '.' );
+
+        # add it to ticket history
+        $Self->HistoryAdd(
+            TicketID     => $Param{TicketID},
+            CreateUserID => $Param{UserID},
+            HistoryType  => 'Misc',
+            Name         => $ErrorMessage,
+        );
+
+        # log
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => $ErrorMessage,
+        );
+    }
 
     return 1;
 }

Kernel/System/CheckItem.pm
@@ -86,7 +86,9 @@ returns true if check was successful, if it's false, get the error message
 from CheckError()
 
     my $Valid = $CheckItemObject->CheckEmail(
-        Address => 'info@example.com',
+        Address       => 'info@example.com',
+        SkipDNSChecks => 0, # 0 - do DNS checks if enabled in SysConfig with CheckMXRecord (default)
+                            # 1 - skip DNS checks even if enabled in SysConfig with CheckMXRecord
     );
 
 =cut
@@ -101,6 +103,11 @@ sub CheckEmail {
             Message  => 'Need Address!'
         );
         return;
+    }
+
+    # do DNS check by default if not disabled
+    if ( !defined $Param{SkipDNSChecks} || $Param{SkipDNSChecks} ne '1' ) {
+        $Param{SkipDNSChecks} = 0;
     }
 
     # get config object
@@ -132,7 +139,8 @@ sub CheckEmail {
 
     # mx check
     elsif (
-        $ConfigObject->Get('CheckMXRecord')
+        !$Param{SkipDNSChecks}
+        && $ConfigObject->Get('CheckMXRecord')
         && eval { require Net::DNS }    ## no critic
         )
     {
@@ -196,7 +204,7 @@ sub CheckEmail {
             }
         }
     }
-    elsif ( $ConfigObject->Get('CheckMXRecord') ) {
+    elsif ( !$Param{SkipDNSChecks} && $ConfigObject->Get('CheckMXRecord') ) {
 
         $Kernel::OM->Get('Kernel::System::Log')->Log(
             Priority => 'error',

commit 9df68240082618c5787b741f272234755bc2b7c1

Fixed quotation in log message
Fixed quotation in log message.

Fixes: 8569786ee178f8de77aa3c737d92fff2bbc33330
Related: https://dev.ib.pl/ib/otrs/issues/101
Author-Change-Id: IB#1023814

Kernel/System/Ticket/Article.pm
@@ -2290,7 +2290,7 @@ sub ArticleSend {
         Priority => 'info',
         Message  => "Sent email to '$ToOrig' "
             . ( $CcOrig ? "(cc '$CcOrig') " : '' )
-            . "from '$Param{From} (Message-ID: $MessageID)'. "
+            . "from '$Param{From}' (Message-ID: $MessageID). "
             . "HistoryType => $HistoryType, Subject => $Param{Subject};",
     );
 