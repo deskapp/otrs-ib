deskapp/otrs-ib

commit 3dd0e1181d448a35edb9aac499993e6ed6a1801d

Counters incremented transactionally in database
OTRS uses text file to store counter for ticket numbers (in AutoIncrement,
Date and DateChecksum modules). This is file is not protected with locking
during updates which may cause ugly race conditions (concurrent processes
trying to generate new ticket number) and errors like one below caught
in OTRS 3:

    DBD::mysql::db do failed: Duplicate entry '100000034' for key 'ticket_tn' at /opt/otrs/Kernel/System/DB.pm line 475.
    ERROR: OTRS-otrs.PostMasterDaemon.pl-4 Perl: 5.14.2 OS: linux Time: Sun Oct 20 11:23:34 2013

Managing counter outside of database is not elegant nor easy
to maintain in cluster environment. Counters should be incremented
and read transactionally with locking to avoid races.

This mod extends OTRS interface to DBI with transaction handling
and introduces new counter module AutoIncrementDB
for Ticket::NumberGenerator setting in SysConfig, which stores
counter in new SQL table otrs.counter and updates it transactionally.

Related: https://dev.ib.pl/ib/otrs/issues/106
Author-Change-Id: IB#1015965

scripts/DBUpdate-to-5.pl
@@ -137,6 +137,10 @@ Please run it as the 'otrs' user or with the help of su:
             Command => \&_UninstallMergedFeatureAddOns,
         },
         {
+            Message => 'Initializing counters',
+            Command => \&_InitializeCounters,
+        },
+        {
             Message => 'Clean up the cache',
             Command => sub {
                 $Kernel::OM->Get('Kernel::System::Cache')->CleanUp();
@@ -3219,6 +3223,22 @@ sub _FixNotificationTags {
     }
 
     return 1;
+}
+
+=item _InitializeCounters()
+
+OTRS 5 introduces counters in DB.
+
+=cut
+
+sub _InitializeCounters {
+
+    # Initialize TicketNumber counter
+    my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+    return if !$DBObject->Do(
+        SQL  => "INSERT INTO counter (id, name, value, create_by, create_time, change_by, change_time) "
+            . " VALUES (1, 'TicketNumber', 0, 1, current_timestamp, 1, current_timestamp)"
+    );
 }
 
 1;

Kernel/Config/Files/Ticket.xml
@@ -219,7 +219,7 @@
         </Setting>
     </ConfigItem>
     <ConfigItem Name="Ticket::NumberGenerator" Required="1" Valid="1">
-        <Description Translatable="1">Selects the ticket number generator module. "AutoIncrement" increments the ticket number, the SystemID and the counter are used with SystemID.counter format (e.g. 1010138, 1010139). With "Date" the ticket numbers will be generated by the current date, the SystemID and the counter. The format looks like Year.Month.Day.SystemID.counter (e.g. 200206231010138, 200206231010139). With "DateChecksum"  the counter will be appended as checksum to the string of date and SystemID. The checksum will be rotated on a daily basis. The format looks like Year.Month.Day.SystemID.Counter.CheckSum (e.g. 2002070110101520, 2002070110101535). "Random" generates randomized ticket numbers in the format "SystemID.Random" (e.g. 100057866352, 103745394596).</Description>
+        <Description Translatable="1">Selects the ticket number generator module. "AutoIncrement" increments the ticket number, the SystemID and the counter are used with SystemID.counter format (e.g. 1010138, 1010139). AutoIncrementDB is the same as AutoIncelement but counter is kept in SQL database, not in text file. With "Date" the ticket numbers will be generated by the current date, the SystemID and the counter. The format looks like Year.Month.Day.SystemID.counter (e.g. 200206231010138, 200206231010139). With "DateChecksum"  the counter will be appended as checksum to the string of date and SystemID. The checksum will be rotated on a daily basis. The format looks like Year.Month.Day.SystemID.Counter.CheckSum (e.g. 2002070110101520, 2002070110101535). "Random" generates randomized ticket numbers in the format "SystemID.Random" (e.g. 100057866352, 103745394596).</Description>
         <Group>Ticket</Group>
         <SubGroup>Core::Ticket</SubGroup>
         <Setting>
@@ -238,7 +238,7 @@
         </Setting>
     </ConfigItem>
     <ConfigItem Name="Ticket::NumberGenerator::MinCounterSize" Required="1" Valid="1">
-        <Description Translatable="1">Sets the minimal ticket counter size if "AutoIncrement" was selected as TicketNumberGenerator. Default is 5, this means the counter starts from 10000.</Description>
+        <Description Translatable="1">Sets the minimal ticket counter size if "AutoIncrement" or "AutoIncrementDB" was selected as TicketNumberGenerator. Default is 5, this means the counter starts from 10000.</Description>
         <Group>Ticket</Group>
         <SubGroup>Core::Ticket</SubGroup>
         <Setting>

Kernel/System/Counter.pm
@@ -1,0 +1,161 @@
+# --
+# Copyright (C) 2016 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Counter;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::System::DB',
+    'Kernel::System::Log',
+);
+
+=head1 NAME
+
+Kernel::System::Counter - counter handling lib
+
+=head1 DESCRIPTION
+
+All counter related functions.
+
+=head1 PUBLIC INTERFACE
+
+=head2 new()
+
+Don't use the constructor directly, use the ObjectManager instead:
+
+    my $CounterObject = $Kernel::OM->Get('Kernel::System::Counter');
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+=head2 IncrementAndGet()
+
+Increment counter and return incremented value.
+
+    my $ID = $CounterObject->IncrementAndGet(
+        CounterName => 'DocumentID',
+        Value => 1,   # the amount by which to increment; 1 by default
+        UserID => 123,
+    );
+
+Returns undef on error.
+
+=cut
+
+sub IncrementAndGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(CounterName UserID)) {
+        if ( !$Param{$_} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $_!"
+            );
+            return;
+        }
+    }
+
+    # get increment value; 1 if not specified
+    my $IncrementValue = $Param{Value} // 1;
+
+    # get database object
+    my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+
+    # increment counter and return incremented value in one transaction
+    # to avoid race conditions
+
+    # counter value to return
+    my $CounterValue;
+
+    # enable rising errors during transaction
+    $DBObject->SetRaiseError( Enabled => 1 );
+
+    eval {
+
+        # other processes accessing ticket counter must wait for this transaction to finish
+        $DBObject->Do(
+            SQL => 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE'
+        );
+
+        $DBObject->BeginWork();
+
+        $DBObject->Do(
+            SQL => 'UPDATE counter set value = value + ?, change_time = current_timestamp, change_by = ? WHERE name = ?',
+            Bind => [ \$IncrementValue, \$Param{UserID}, \$Param{CounterName} ],
+        );
+
+        # uncomment for debugging only (i.e. transaction locks testing)
+        #sleep(30);
+
+        $DBObject->Prepare(
+            SQL => 'SELECT value FROM counter WHERE name = ?',
+            Bind => [ \$Param{CounterName} ],
+        );
+
+        while ( my @Data = $DBObject->FetchrowArray() ) {
+            if ( defined $Data[0] ) {
+                $CounterValue = $Data[0];
+            }
+        }
+
+        die "Cannot read $Param{CounterName} counter from database after incrementing!" if !defined $CounterValue;
+
+        $DBObject->Commit();
+    };
+
+    if ( $@ ) {
+        my $ErrorMessage = $@;
+
+        # append database error message if available
+        if ( my $DBErrorMessage = $DBObject->Error() ) {
+            $ErrorMessage .= $ErrorMessage ? ', ' : '';
+            $ErrorMessage .= $DBErrorMessage;
+        }
+
+        eval { $DBObject->Rollback(); };
+
+        # disable rising errors after transaction
+        $DBObject->SetRaiseError( Enabled => 0 );
+
+        $ErrorMessage = "Error incrementing $Param{CounterName} counter in database"
+            . ( $ErrorMessage ? ': ' . $ErrorMessage : '' );
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => $ErrorMessage,
+        );
+
+        return;
+    }
+
+    # disable rising errors after transaction
+    $DBObject->SetRaiseError( Enabled => 0 );
+
+    return $CounterValue;
+}
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut

Kernel/System/DB.pm
@@ -371,7 +371,7 @@ to retrieve database errors
 sub Error {
     my $Self = shift;
 
-    return $DBI::errstr;
+    return $DBI::err ? $DBI::errstr : '';
 }
 
 =item Do()
@@ -1627,6 +1627,94 @@ sub Ping {
     }
 
     return $Self->{dbh}->ping();
+}
+
+=item BeginWork()
+
+    my $Success = $DBObject->BeginWork()
+
+Starts a database transaction.
+
+=cut
+
+sub BeginWork {
+    my ( $Self, %Param ) = @_;
+
+    return if !$Self->Connect();
+
+    # start transaction
+    return $Self->{dbh}->begin_work();
+}
+
+=item Commit()
+
+    my $Success = $DBObject->Commit()
+
+Commits a database transaction.
+
+=cut
+
+sub Commit {
+    my ( $Self, %Param ) = @_;
+
+    # commit transaction
+    return $Self->{dbh}->commit();
+}
+
+=item Rollback()
+
+    my $Success = $DBObject->Rollback()
+
+Rolls back a database transaction.
+
+=cut
+
+sub Rollback {
+    my ( $Self, %Param ) = @_;
+
+    # rollback transaction
+    return $Self->{dbh}->rollback();
+}
+
+=item GetRaiseError()
+
+    my $RaiseError = $DBObject->GetRaiseError()
+
+Gets current RaiseError value.
+
+=cut
+
+sub GetRaiseError {
+    my ( $Self, %Param ) = @_;
+
+    return $Self->{dbh}->{RaiseError};
+}
+
+=item SetRaiseError()
+
+    my $Success = $DBObject->SetRaiseError(
+        Enabled => 0;  # 0 - disable raising errors; 1 - to enable
+    );
+
+Enabled or disables raising errors.
+
+=cut
+
+sub SetRaiseError {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( ! defined $Param{Enabled} ) {
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'error',
+            Message  => "Need Enabled!"
+        );
+        return;
+    }
+
+    $Self->{dbh}->{RaiseError} = $Param{Enabled};
+
+    return 1;
 }
 
 =begin Internal:

Kernel/System/Ticket/Number/AutoIncrementDB.pm
@@ -1,0 +1,135 @@
+# --
+# Copyright (C) 2016 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
+# Based on AutoIncrement.pm by OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+#
+# Generates autoincremented ticket numbers (like 1010138, 1010139, ...).
+# Counter is being stored in DB.
+# --
+
+package Kernel::System::Ticket::Number::AutoIncrementDB;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::Log',
+    'Kernel::System::Main',
+    'Kernel::System::Ticket',
+    'Kernel::System::Counter',
+);
+
+sub new {
+    my ($Type) = @_;
+
+    my $Self = {};
+    return bless( $Self, $Type );
+}
+
+sub TicketCreateNumber {
+    my ( $Self, $JumpCounter ) = @_;
+
+    if ( !$JumpCounter ) {
+        $JumpCounter = 0;
+    }
+
+    # get needed objects
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
+    my $CounterObject = $Kernel::OM->Get('Kernel::System::Counter');
+
+    # get needed config options
+    my $CounterLog = $ConfigObject->Get('Ticket::CounterLog');
+    my $SystemID = $ConfigObject->Get('SystemID');
+    my $MinSize = $ConfigObject->Get('Ticket::NumberGenerator::AutoIncrement::MinCounterSize')
+        || $ConfigObject->Get('Ticket::NumberGenerator::MinCounterSize')
+        || 5;
+
+    # get next TicketNumber counter value for new ticket
+    my $Count = $CounterObject->IncrementAndGet(
+        CounterName => 'TicketNumber',
+        Value => 1 + $JumpCounter,
+        UserID => 1,
+    );
+
+    # abort if counter value cannot be obtained
+    return if !defined $Count;
+
+    # pad ticket number with leading '0' to length $MinSize (config option)
+    $Count = sprintf "%.*u", $MinSize, $Count;
+
+    # create new ticket number
+    my $Tn = $SystemID . $Count;
+
+    # Check ticket number. If exists generate new one!
+    if ( $Kernel::OM->Get('Kernel::System::Ticket')->TicketCheckNumber( Tn => $Tn ) ) {
+
+        $Self->{LoopProtectionCounter}++;
+
+        if ( $Self->{LoopProtectionCounter} >= 16000 ) {
+
+            # loop protection
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "CounterLoopProtection is now $Self->{LoopProtectionCounter}!"
+                    . " Stopped TicketCreateNumber()!",
+            );
+            return;
+        }
+
+        # create new ticket number again
+        $Kernel::OM->Get('Kernel::System::Log')->Log(
+            Priority => 'notice',
+            Message  => "Tn ($Tn) exists! Creating a new one.",
+        );
+
+        $Tn = $Self->TicketCreateNumber( $Self->{LoopProtectionCounter} );
+    }
+
+    return $Tn;
+}
+
+sub GetTNByString {
+    my ( $Self, $String ) = @_;
+
+    if ( !$String ) {
+        return;
+    }
+
+    # get config object
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    # get needed config options
+    my $CheckSystemID = $ConfigObject->Get('Ticket::NumberGenerator::CheckSystemID');
+    my $SystemID      = '';
+
+    if ($CheckSystemID) {
+        $SystemID = $ConfigObject->Get('SystemID');
+    }
+
+    my $TicketHook        = $ConfigObject->Get('Ticket::Hook');
+    my $TicketHookDivider = $ConfigObject->Get('Ticket::HookDivider');
+    my $MinSize           = $ConfigObject->Get('Ticket::NumberGenerator::AutoIncrement::MinCounterSize')
+        || $ConfigObject->Get('Ticket::NumberGenerator::MinCounterSize')
+        || 5;
+    my $MaxSize = $MinSize + 5;
+
+    # check ticket number
+    if ( $String =~ /\Q$TicketHook$TicketHookDivider\E($SystemID\d{$MinSize,$MaxSize})/i ) {
+        return $1;
+    }
+
+    if ( $String =~ /\Q$TicketHook\E:\s{0,2}($SystemID\d{$MinSize,$MaxSize})/i ) {
+        return $1;
+    }
+
+    return;
+}
+
+1;

scripts/DBUpdate-to-5.mysql.sql
@@ -98,7 +98,23 @@ CREATE TABLE cloud_service_config (
     UNIQUE INDEX cloud_service_config_config_md5 (config_md5),
     UNIQUE INDEX cloud_service_config_name (name)
 );
+# ----------------------------------------------------------
+#  create table counter
+# ----------------------------------------------------------
+CREATE TABLE counter (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR (64) NOT NULL,
+    value BIGINT NOT NULL,
+    create_time DATETIME NOT NULL,
+    create_by INTEGER NOT NULL,
+    change_time DATETIME NOT NULL,
+    change_by INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE INDEX counter_name (name)
+);
 ALTER TABLE notification_event_message ADD CONSTRAINT FK_notification_event_message_notification_id_id FOREIGN KEY (notification_id) REFERENCES notification_event (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_create_by_id FOREIGN KEY (create_by) REFERENCES users (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_change_by_id FOREIGN KEY (change_by) REFERENCES users (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_valid_id_id FOREIGN KEY (valid_id) REFERENCES valid (id);
+ALTER TABLE counter ADD CONSTRAINT FK_counter_create_by_id FOREIGN KEY (create_by) REFERENCES users (id);
+ALTER TABLE counter ADD CONSTRAINT FK_counter_change_by_id FOREIGN KEY (change_by) REFERENCES users (id);

scripts/DBUpdate-to-5.oracle.sql
@@ -233,9 +233,53 @@ END;
 CREATE INDEX FK_cloud_service_config_chane1 ON cloud_service_config (change_by);
 CREATE INDEX FK_cloud_service_config_crea30 ON cloud_service_config (create_by);
 CREATE INDEX FK_cloud_service_config_valib2 ON cloud_service_config (valid_id);
+-- ----------------------------------------------------------
+--  create table counter
+-- ----------------------------------------------------------
+CREATE TABLE counter (
+    id NUMBER (12, 0) NOT NULL,
+    name VARCHAR2 (64) NOT NULL,
+    value NUMBER (20, 0) NOT NULL,
+    create_time DATE NOT NULL,
+    create_by NUMBER (12, 0) NOT NULL,
+    change_time DATE NOT NULL,
+    change_by NUMBER (12, 0) NOT NULL,
+    CONSTRAINT counter_name UNIQUE (name)
+);
+ALTER TABLE counter ADD CONSTRAINT PK_counter PRIMARY KEY (id);
+BEGIN
+  EXECUTE IMMEDIATE 'DROP SEQUENCE SE_counter';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
+/
+--;
+CREATE SEQUENCE SE_counter
+INCREMENT BY 1
+START WITH 1
+NOMAXVALUE
+NOCYCLE
+CACHE 20
+ORDER;
+CREATE OR REPLACE TRIGGER SE_counter_t
+BEFORE INSERT ON counter
+FOR EACH ROW
+BEGIN
+  IF :new.id IS NULL THEN
+    SELECT SE_counter.nextval
+    INTO :new.id
+    FROM DUAL;
+  END IF;
+END;
+/
+--;
+CREATE INDEX FK_counter_change_by ON counter (change_by);
+CREATE INDEX FK_counter_create_by ON counter (create_by);
 SET DEFINE OFF;
 SET SQLBLANKLINES ON;
 ALTER TABLE notification_event_message ADD CONSTRAINT FK_notification_event_messag16 FOREIGN KEY (notification_id) REFERENCES notification_event (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_creafe FOREIGN KEY (create_by) REFERENCES users (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_chan63 FOREIGN KEY (change_by) REFERENCES users (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_vali9c FOREIGN KEY (valid_id) REFERENCES valid (id);
+ALTER TABLE counter ADD CONSTRAINT FK_counter_create_by_id FOREIGN KEY (create_by) REFERENCES users (id);
+ALTER TABLE counter ADD CONSTRAINT FK_counter_change_by_id FOREIGN KEY (change_by) REFERENCES users (id);

scripts/DBUpdate-to-5.postgresql.sql
@@ -99,8 +99,24 @@ CREATE TABLE cloud_service_config (
     CONSTRAINT cloud_service_config_config_md5 UNIQUE (config_md5),
     CONSTRAINT cloud_service_config_name UNIQUE (name)
 );
+-- ----------------------------------------------------------
+--  create table counter
+-- ----------------------------------------------------------
+CREATE TABLE counter (
+    id serial NOT NULL,
+    name VARCHAR (64) NOT NULL,
+    value BIGINT NOT NULL,
+    create_time timestamp(0) NOT NULL,
+    create_by INTEGER NOT NULL,
+    change_time timestamp(0) NOT NULL,
+    change_by INTEGER NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT counter_name UNIQUE (name)
+);
 SET standard_conforming_strings TO ON;
 ALTER TABLE notification_event_message ADD CONSTRAINT FK_notification_event_message_notification_id_id FOREIGN KEY (notification_id) REFERENCES notification_event (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_create_by_id FOREIGN KEY (create_by) REFERENCES users (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_change_by_id FOREIGN KEY (change_by) REFERENCES users (id);
 ALTER TABLE cloud_service_config ADD CONSTRAINT FK_cloud_service_config_valid_id_id FOREIGN KEY (valid_id) REFERENCES valid (id);
+ALTER TABLE counter ADD CONSTRAINT FK_counter_create_by_id FOREIGN KEY (create_by) REFERENCES users (id);
+ALTER TABLE counter ADD CONSTRAINT FK_counter_change_by_id FOREIGN KEY (change_by) REFERENCES users (id);

commit fcc58640db8abb196b7ce0ffc7cb0cea5491bb52

Added missing return status
Fixed problem in the counter initialization step in upgrade
script.

Fixes: 3dd0e1181d448a35edb9aac499993e6ed6a1801d
Related: https://dev.ib.pl/ib/otrs/issues/106
Author-Change-Id: IB#1015965

scripts/DBUpdate-to-5.pl
@@ -3239,6 +3239,8 @@ sub _InitializeCounters {
         SQL  => "INSERT INTO counter (id, name, value, create_by, create_time, change_by, change_time) "
             . " VALUES (1, 'TicketNumber', 0, 1, current_timestamp, 1, current_timestamp)"
     );
+
+    return 1;
 }
 
 1;