deskapp/otrs-ib

commit c38803f3ac97f24ddf237f2f0d14798a3d2d9bbd

HTML table creating and pasting added, paste as plain text added
OTRS rich text editor (ckeditor) does not handle HTML tables
in standard mode (Frontend::RichText::EnhancedMode=0).
This mod adds table function to standard mode and allows
OTRS to preserve font tags and table row colors set using
bgcolor.

This mod also adds to editor in standard mode PasteText
function to allow easier pasting plain text into editor
in Firefox (there is a problem with this function in Firefox
and newer ckeditor versions - see
https://bugzilla.mozilla.org/show_bug.cgi?id=1342189).

Related: https://dev.ib.pl/ib/otrs/issues/107
Related: https://bugzilla.mozilla.org/show_bug.cgi?id=1342189
Author-Change-Id: IB#1067151

var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -142,7 +142,7 @@ Core.UI.RichTextEditor = (function (TargetNS) {
             forcePasteAsPlainText: false,
             format_tags: 'p;h1;h2;h3;h4;h5;h6;pre',
             fontSize_sizes: '8px;10px;12px;16px;18px;20px;22px;24px;26px;28px;30px;',
-            extraAllowedContent: 'div[type]{*}; img[*]; col[width]; style[*]{*}; *[id](*)',
+            extraAllowedContent: 'div[type]{*}; img[*]; col[width]; style[*]{*}; *[id](*); font[*]; *[bgcolor](*)',
             enterMode: CKEDITOR.ENTER_BR,
             shiftEnterMode: CKEDITOR.ENTER_BR,
             contentsLangDirection: Core.Config.Get('RichText.TextDir', 'ltr'),

Kernel/Output/HTML/Templates/Standard/RichTextEditor.tt.orig
@@ -48,12 +48,12 @@
     }
     else {
         Core.Config.Set('RichText.Toolbar', [
-            ['Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Link', 'Unlink', '-', 'Image', 'HorizontalRule', 'Table', '-', 'Undo', 'Redo', '-', 'Find', 'SpellCheck', 'PasteText' ],
+            ['Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Link', 'Unlink', '-', 'Image', 'HorizontalRule', '-', 'Undo', 'Redo', '-', 'Find', 'SpellCheck'],
             '/',
             ['Format', 'Font', 'FontSize', '-', 'TextColor', 'BGColor', 'RemoveFormat', '-', 'Source', 'SpecialChar', 'SplitQuote', 'RemoveQuote', '-', 'Maximize']
         ]);
         Core.Config.Set('RichText.ToolbarWithoutImage', [
-            ['Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Link', 'Unlink', '-', 'HorizontalRule', 'Table', '-', 'Undo', 'Redo', '-', 'Find', 'SpellCheck', 'PasteText' ],
+            ['Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Link', 'Unlink', '-', 'HorizontalRule', '-', 'Undo', 'Redo', '-', 'Find', 'SpellCheck'],
             '/',
             ['Format', 'Font', 'FontSize', '-', 'TextColor', 'BGColor', 'RemoveFormat', '-', 'Source', 'SpecialChar', 'SplitQuote', 'RemoveQuote', '-', 'Maximize']
         ]);

commit aadc19d076e29d53609deeface955bd57deefd79

HTML table handling enhanced
HTML table handling enhanced (force ckeditor not to drop table formatting).

Fixes: c38803f3ac97f24ddf237f2f0d14798a3d2d9bbd
Related: https://dev.ib.pl/ib/otrs/issues/107
Author-Change-Id: IB#1067151

var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -142,7 +142,7 @@ Core.UI.RichTextEditor = (function (TargetNS) {
             forcePasteAsPlainText: false,
             format_tags: 'p;h1;h2;h3;h4;h5;h6;pre',
             fontSize_sizes: '8px;10px;12px;16px;18px;20px;22px;24px;26px;28px;30px;',
-            extraAllowedContent: 'div[type]{*}; img[*]; col[width]; style[*]{*}; *[id](*); font[*]; *[bgcolor](*)',
+            extraAllowedContent: 'div[type]{*}; img[*]; style[*]{*}; *[id](*); font[*]; *[bgcolor](*); table[*]; th[*]; tr[*]; td[*]; colgroup[*]; col[*];',
             enterMode: CKEDITOR.ENTER_BR,
             shiftEnterMode: CKEDITOR.ENTER_BR,
             contentsLangDirection: Core.Config.Get('RichText.TextDir', 'ltr'),