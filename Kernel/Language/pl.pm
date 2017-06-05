337140067d87bb23e43dd12b350367cb9d56fefe
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2017-06-05 17:17:54 +0200

    PL translation update
    PL translation update.
    
    Fixes: 47a5c87f0f76acbee0455e753cc60c3223b0af3d
    Related: https://dev.ib.pl/ib/otrs/issues/108
    Author-Change-Id: IB#1023276

--- Kernel/Language/pl.pm
+++ Kernel/Language/pl.pm
@@ -6999,6 +6999,7 @@ Thanks for your help!
         'CreateTicket' => 'Nowe zgłoszenie',
         'TicketZoom' => 'Podgląd zgłoszenia',
         'PhoneCall' => 'Połączenie telefoniczne',
+        'Customer tickets' => 'Zgłoszenia klienta',
 
     };
     # $$STOP$$
