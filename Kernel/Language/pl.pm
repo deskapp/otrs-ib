deskapp/otrs-ib

commit 0f500851fa716df5c54db7ee4975d2542700d13d

PDF attachment name formatting and printout translation update
Nicer PDF attachment name formatting and ticket printout
function Polish translation added.

Fixes: f1b0711d795df8d55bcd9124fa1eb2217438d0be
Related: https://dev.ib.pl/ib/otrs/issues/3
Author-Change-Id: IB#1027611

Kernel/Language/pl.pm.rej
@@ -1,0 +1,11 @@
+--- Kernel/Language/pl.pm	2017-08-03 08:36:23.796000000 +0200
++++ Kernel/Language/pl.pm	2017-08-03 08:57:30.728000000 +0200
+@@ -7112,6 +7112,8 @@
+         'TicketZoom' => 'Podgląd zgłoszenia',
+         'PhoneCall' => 'Połączenie telefoniczne',
+         'Customer tickets' => 'Zgłoszenia klienta',
++        'Warning: printout will include internal information also!' => 'Uwaga: wydruk obejmie również informacje wewnętrzne!',
++        'Attach ticket printout' => 'Załącz wydruk zgłoszenia',
+ 
+     };
+     # $$STOP$$

Kernel/Modules/AgentTicketCompose.pm
@@ -535,7 +535,7 @@ sub Run {
                 }
 
                 # return the pdf document
-                my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+                my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
                 my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
                     SystemTime => $TimeObject->SystemTime(),
                 );
@@ -554,7 +554,7 @@ sub Run {
                     $UploadCacheObject->FormIDAddFile(
                         FormID => $GetParam{FormID},
                         Disposition => 'attachment',
-                        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+                        Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
                         Content     => $PDFString,
                         ContentType => 'application/pdf',
                     );

Kernel/Modules/AgentTicketForward.pm
@@ -914,7 +914,7 @@ sub SendEmail {
             }
 
             # return the pdf document
-            my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+            my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
             my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
                 SystemTime => $TimeObject->SystemTime(),
             );
@@ -933,7 +933,7 @@ sub SendEmail {
                 $UploadCacheObject->FormIDAddFile(
                     FormID => $GetParam{FormID},
                     Disposition => 'attachment',
-                    Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+                    Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
                     Content     => $PDFString,
                     ContentType => 'application/pdf',
                 );

Kernel/Modules/AgentTicketPrint.pm
@@ -84,12 +84,13 @@ sub Run {
 
     # get needed objects
     my $TimeObject = $Kernel::OM->Get('Kernel::System::Time');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 
     # get time object and use the UserTimeObject, if the system use UTC as
     # system time and the TimeZoneUser feature is active
     if (
         !$Kernel::OM->Get('Kernel::System::Time')->ServerLocalTimeOffsetSeconds()
-        && $Kernel::OM->Get('Kernel::Config')->Get('TimeZoneUser')
+        && $ConfigObject->Get('TimeZoneUser')
         && $Self->{UserTimeZone}
         )
     {
@@ -97,7 +98,7 @@ sub Run {
     }
 
     # return the pdf document
-    my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+    my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
     my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
         SystemTime => $TimeObject->SystemTime(),
     );
@@ -113,7 +114,7 @@ sub Run {
     );
 
     return $LayoutObject->Attachment(
-        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+        Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
         ContentType => "application/pdf",
         Content     => $PDFString,
         Type        => 'inline',

Kernel/Modules/CustomerTicketPrint.pm
@@ -223,7 +223,7 @@ sub Run {
     );
 
     # return the pdf document
-    my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+    my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
     my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
         SystemTime => $TimeObject->SystemTime(),
     );
@@ -233,7 +233,7 @@ sub Run {
     $m = sprintf( "%02d", $m );
     my $PDFString = $PDFObject->DocumentOutput();
     return $LayoutObject->Attachment(
-        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+        Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
         ContentType => "application/pdf",
         Content     => $PDFString,
         Type        => 'inline',

commit cbc51ede4768230c0e2b84230b1016fe013df34b

Polish translation fix
Removed unnecessarry file and Polish translation fix.

Fixes: 0f500851fa716df5c54db7ee4975d2542700d13d
Related: https://dev.ib.pl/ib/otrs/issues/3
Author-Change-Id: IB#1027611

Kernel/Language/pl.pm
@@ -6819,6 +6819,8 @@ Thanks for your help!
         'info' => 'informacja',
         'inline' => '',
         'notice' => 'ogłoszenie',
+        'Warning: printout will include internal information also!' => 'Uwaga: wydruk obejmie również informacje wewnętrzne!',
+        'Attach ticket printout' => 'Załącz wydruk zgłoszenia',
 
     };
     # $$STOP$$

Kernel/Language/pl.pm.rej
@@ -1,11 +1,0 @@
---- Kernel/Language/pl.pm	2017-08-03 08:36:23.796000000 +0200
-+++ Kernel/Language/pl.pm	2017-08-03 08:57:30.728000000 +0200
-@@ -7112,6 +7112,8 @@
-         'TicketZoom' => 'Podgląd zgłoszenia',
-         'PhoneCall' => 'Połączenie telefoniczne',
-         'Customer tickets' => 'Zgłoszenia klienta',
-+        'Warning: printout will include internal information also!' => 'Uwaga: wydruk obejmie również informacje wewnętrzne!',
-+        'Attach ticket printout' => 'Załącz wydruk zgłoszenia',
- 
-     };
-     # $$STOP$$