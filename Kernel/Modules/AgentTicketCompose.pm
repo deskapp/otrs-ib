deskapp/otrs-ib

commit f1b0711d795df8d55bcd9124fa1eb2217438d0be

Fixed time handling routines copied from OTRS 6
Fixed time handling in simple ticket printout attaching mod
introduced in e4b1555ba9515d0a46a860564c70c50edb3b1230 (was
copies from OTRS 6 code by mistake).

Fixes: e4b1555ba9515d0a46a860564c70c50edb3b1230
Related: https://dev.ib.pl/ib/otrs/issues/3
Related: https://github.com/OTRS/otrs/pull/266
Author-Change-Id: IB#1027611

Kernel/Modules/AgentTicketCompose.pm
@@ -523,14 +523,26 @@ sub Run {
                 %Error = ();
                 $Error{AttachmentAddTicketPrintout} = 1;
 
-                # assemble file name
-                my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
-                if ( $Self->{UserTimeZone} ) {
-                    $DateTimeObject->ToTimeZone( TimeZone => $Self->{UserTimeZone} );
+                # get time object and use the UserTimeObject, if the system use UTC as
+                # system time and the TimeZoneUser feature is active
+                if (
+                    !$Kernel::OM->Get('Kernel::System::Time')->ServerLocalTimeOffsetSeconds()
+                    && $Kernel::OM->Get('Kernel::Config')->Get('TimeZoneUser')
+                    && $Self->{UserTimeZone}
+                    )
+                {
+                    $TimeObject = $LayoutObject->{UserTimeObject};
                 }
-                my $Filename = 'Ticket_' . $Tn . '_';
-                $Filename .= $DateTimeObject->Format( Format => '%Y-%m-%d_%H:%M' );
-                $Filename .= '.pdf';
+
+                # return the pdf document
+                my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+                my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
+                    SystemTime => $TimeObject->SystemTime(),
+                );
+                $M = sprintf( "%02d", $M );
+                $D = sprintf( "%02d", $D );
+                $h = sprintf( "%02d", $h );
+                $m = sprintf( "%02d", $m );
 
                 # generate pdf printout
                 my $PDFString = $Kernel::OM->Get('Kernel::Output::PDF::Ticket')->GenerateAgentPDF(
@@ -542,7 +554,7 @@ sub Run {
                     $UploadCacheObject->FormIDAddFile(
                         FormID => $GetParam{FormID},
                         Disposition => 'attachment',
-                        Filename    => $Filename,
+                        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
                         Content     => $PDFString,
                         ContentType => 'application/pdf',
                     );

Kernel/Modules/AgentTicketForward.pm
@@ -902,14 +902,26 @@ sub SendEmail {
             %Error = ();
             $Error{AttachmentAddTicketPrintout} = 1;
 
-            # assemble file name
-            my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
-            if ( $Self->{UserTimeZone} ) {
-                $DateTimeObject->ToTimeZone( TimeZone => $Self->{UserTimeZone} );
+            # get time object and use the UserTimeObject, if the system use UTC as
+            # system time and the TimeZoneUser feature is active
+            if (
+                !$Kernel::OM->Get('Kernel::System::Time')->ServerLocalTimeOffsetSeconds()
+                && $Kernel::OM->Get('Kernel::Config')->Get('TimeZoneUser')
+                && $Self->{UserTimeZone}
+                )
+            {
+                $TimeObject = $LayoutObject->{UserTimeObject};
             }
-            my $Filename = 'Ticket_' . $Tn . '_';
-            $Filename .= $DateTimeObject->Format( Format => '%Y-%m-%d_%H:%M' );
-            $Filename .= '.pdf';
+
+            # return the pdf document
+            my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+            my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
+                SystemTime => $TimeObject->SystemTime(),
+            );
+            $M = sprintf( "%02d", $M );
+            $D = sprintf( "%02d", $D );
+            $h = sprintf( "%02d", $h );
+            $m = sprintf( "%02d", $m );
 
             # generate pdf printout
             my $PDFString = $Kernel::OM->Get('Kernel::Output::PDF::Ticket')->GenerateAgentPDF(
@@ -921,7 +933,7 @@ sub SendEmail {
                 $UploadCacheObject->FormIDAddFile(
                     FormID => $GetParam{FormID},
                     Disposition => 'attachment',
-                    Filename    => $Filename,
+                    Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
                     Content     => $PDFString,
                     ContentType => 'application/pdf',
                 );

commit 0f500851fa716df5c54db7ee4975d2542700d13d

PDF attachment name formatting and printout translation update
Nicer PDF attachment name formatting and ticket printout
function Polish translation added.

Fixes: f1b0711d795df8d55bcd9124fa1eb2217438d0be
Related: https://dev.ib.pl/ib/otrs/issues/3
Author-Change-Id: IB#1027611

Kernel/Language/pl.pm.rej
@@ -1,0 +1,11 @@
+--- Kernel/Language/pl.pm	2017-08-03 08:36:23.796000000 +0200
++++ Kernel/Language/pl.pm	2017-08-03 08:57:30.728000000 +0200
+@@ -7112,6 +7112,8 @@
+         'TicketZoom' => 'Podgląd zgłoszenia',
+         'PhoneCall' => 'Połączenie telefoniczne',
+         'Customer tickets' => 'Zgłoszenia klienta',
++        'Warning: printout will include internal information also!' => 'Uwaga: wydruk obejmie również informacje wewnętrzne!',
++        'Attach ticket printout' => 'Załącz wydruk zgłoszenia',
+ 
+     };
+     # $$STOP$$

Kernel/Modules/AgentTicketCompose.pm
@@ -535,7 +535,7 @@ sub Run {
                 }
 
                 # return the pdf document
-                my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+                my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
                 my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
                     SystemTime => $TimeObject->SystemTime(),
                 );
@@ -554,7 +554,7 @@ sub Run {
                     $UploadCacheObject->FormIDAddFile(
                         FormID => $GetParam{FormID},
                         Disposition => 'attachment',
-                        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+                        Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
                         Content     => $PDFString,
                         ContentType => 'application/pdf',
                     );

Kernel/Modules/AgentTicketForward.pm
@@ -914,7 +914,7 @@ sub SendEmail {
             }
 
             # return the pdf document
-            my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+            my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
             my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
                 SystemTime => $TimeObject->SystemTime(),
             );
@@ -933,7 +933,7 @@ sub SendEmail {
                 $UploadCacheObject->FormIDAddFile(
                     FormID => $GetParam{FormID},
                     Disposition => 'attachment',
-                    Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+                    Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
                     Content     => $PDFString,
                     ContentType => 'application/pdf',
                 );

Kernel/Modules/AgentTicketPrint.pm
@@ -84,12 +84,13 @@ sub Run {
 
     # get needed objects
     my $TimeObject = $Kernel::OM->Get('Kernel::System::Time');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 
     # get time object and use the UserTimeObject, if the system use UTC as
     # system time and the TimeZoneUser feature is active
     if (
         !$Kernel::OM->Get('Kernel::System::Time')->ServerLocalTimeOffsetSeconds()
-        && $Kernel::OM->Get('Kernel::Config')->Get('TimeZoneUser')
+        && $ConfigObject->Get('TimeZoneUser')
         && $Self->{UserTimeZone}
         )
     {
@@ -97,7 +98,7 @@ sub Run {
     }
 
     # return the pdf document
-    my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+    my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
     my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
         SystemTime => $TimeObject->SystemTime(),
     );
@@ -113,7 +114,7 @@ sub Run {
     );
 
     return $LayoutObject->Attachment(
-        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+        Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
         ContentType => "application/pdf",
         Content     => $PDFString,
         Type        => 'inline',

Kernel/Modules/CustomerTicketPrint.pm
@@ -223,7 +223,7 @@ sub Run {
     );
 
     # return the pdf document
-    my $Filename = 'Ticket_' . $Ticket{TicketNumber};
+    my $Filename = $ConfigObject->Get('Ticket::Hook') . $Ticket{TicketNumber};
     my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
         SystemTime => $TimeObject->SystemTime(),
     );
@@ -233,7 +233,7 @@ sub Run {
     $m = sprintf( "%02d", $m );
     my $PDFString = $PDFObject->DocumentOutput();
     return $LayoutObject->Attachment(
-        Filename    => $Filename . "_" . "$Y-$M-$D" . "_" . "$h-$m.pdf",
+        Filename    => $Filename . "_" . "$Y$M$D" . "_" . "$h$m.pdf",
         ContentType => "application/pdf",
         Content     => $PDFString,
         Type        => 'inline',