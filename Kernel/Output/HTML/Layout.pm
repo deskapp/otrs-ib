OTRS/otrs

commit 4610a83fd5c61d87c5bc35e41703416235eeb4f9

Revert "Disabled warnings on internal unicode chacter printing, thanks to Pawel Boguslawski."
This reverts commit 593bce9abf7c23db8749b2bb32c4061aef36438b.

Reverted because Perl introduces warning category "nonchar" only with version 5.13.

Kernel/Output/HTML/Layout.pm
@@ -1633,10 +1633,6 @@ sub Print {
         binmode STDOUT, ':bytes';
     }
 
-    # Disable perl warnings in case of printing unicode private chars,
-    #   see https://rt.perl.org/Public/Bug/Display.html?id=121226.
-    no warnings 'nonchar';
-
     print ${ $Param{Output} };
 
     return 1;

deskapp/otrs-ib

commit e63f63cccf68923665d7f22418b66cc27955835e

Required perl version changed to 5.14 and restored unicode warning fix
In 4610a83fd5c61d87c5bc35e41703416235eeb4f9 upstream removed fix introduced
in 3ef9b5660f4bc14af34afb15bdbd46ba647a3e3b because of perl 5.14 requirement.

This restores fix and changes required perl version to 5.14.

Fixes: 4610a83fd5c61d87c5bc35e41703416235eeb4f9
Related: https://dev.ib.pl/ib/otrs/issues/97
Author-Change-Id: IB#1072941

Kernel/Output/HTML/Layout.pm
@@ -1633,6 +1633,10 @@ sub Print {
         binmode STDOUT, ':bytes';
     }
 
+    # Disable perl warnings in case of printing unicode private chars,
+    #   see https://rt.perl.org/Public/Bug/Display.html?id=121226.
+    no warnings 'nonchar';
+
     print ${ $Param{Output} };
 
     return 1;