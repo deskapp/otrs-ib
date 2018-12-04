deskapp/otrs-ib

commit e646974c93f19ed22e20a72ad595b6117150981f

Fixxed infinite loop on filename generation
OTRS falls into an inifinite loop on filename generation
for specific attachments.

Related: https://dev.ib.pl/ib/otrs/issues/118
Author-Change-Id: IB#1085514

Kernel/System/Main.pm
@@ -222,6 +222,11 @@ sub FilenameCleanUp {
         return;
     }
 
+    # escape if cleanup is not needed
+    if ( $Param{NoFilenameClean} ) {
+        return $Param{Filename};
+    }
+
     my $Type = lc( $Param{Type} || 'local' );
 
     if ( $Type eq 'md5' ) {
@@ -232,8 +237,17 @@ sub FilenameCleanUp {
     # replace invalid token for attachment file names
     elsif ( $Type eq 'attachment' ) {
 
-        # replace invalid token like < > ? " : ; | \ / or *
-        $Param{Filename} =~ s/[ <>\?":\\\*\|\/;\[\]]/_/g;
+        # trim whitespace
+        $Param{Filename} =~ s/^\s+|\r|\n|\s+$//g;
+
+        # strip leading dots
+        $Param{Filename} =~ s/^\.+//;
+
+        # only whitelisted characters allowed in filename for security
+        $Param{Filename} =~ s/[^\s\w\-+.#_]/_/g;
+
+        # Enclosed alphanumerics are kept on older Perl versions, make sure to replace them too.
+        $Param{Filename} =~ s/[\x{2460}-\x{24FF}]/_/g;
 
         # replace utf8 and iso
         $Param{Filename} =~ s/(\x{00C3}\x{00A4}|\x{00A4})/ae/g;
@@ -245,19 +259,82 @@ sub FilenameCleanUp {
         $Param{Filename} =~ s/(\x{00C3}\x{009F}|\x{00DF})/ss/g;
         $Param{Filename} =~ s/-+/-/g;
 
-        # cut the string if too long
-        if ( length( $Param{Filename} ) > 100 ) {
-            my $Ext = '';
-            if ( $Param{Filename} =~ /^.*(\.(...|....))$/ ) {
-                $Ext = $1;
+        # separate filename and extension
+        my $FileName = $Param{Filename};
+        my $FileExt  = '';
+        if ( $Param{Filename} =~ /(.*)\.+(.*)$/ ) {
+
+            # allow extensions up to 10 chars to avoid infinite loop below
+            if (length encode('UTF-8', $2) <= 10) {
+                $FileName = $1;
+                $FileExt  = '.' . $2;
             }
-            $Param{Filename} = substr( $Param{Filename}, 0, 95 ) . $Ext;
+
+        }
+
+        if ( length $FileName ) {
+            my $ModifiedName;
+
+            # remove character by character starting from the end of the filename string
+            # untill we get acceptable 220 byte long filename size including extension
+            CHOPSTRING:
+            while (1) {
+
+                $ModifiedName = $FileName . $FileExt;
+
+                last CHOPSTRING if ( length encode( 'UTF-8', $ModifiedName ) < 220 );
+                chop $FileName;
+
+            }
+            $Param{Filename} = $ModifiedName;
         }
     }
     else {
 
-        # replace invalid token like [ ] * : ? " < > ; | \ /
-        $Param{Filename} =~ s/[<>\?":\\\*\|\/;\[\]]/_/g;
+        # trim whitespace
+        $Param{Filename} =~ s/^\s+|\r|\n|\s+$//g;
+
+        # strip leading dots
+        $Param{Filename} =~ s/^\.+//;
+
+        # only whitelisted characters allowed in filename for security
+        if ( !$Param{NoReplace} ) {
+            $Param{Filename} =~ s/[^\w\-+.#_]/_/g;
+
+            # Enclosed alphanumerics are kept on older Perl versions, make sure to replace them too.
+            $Param{Filename} =~ s/[\x{2460}-\x{24FF}]/_/g;
+        }
+
+        # separate filename and extension
+        my $FileName = $Param{Filename};
+        my $FileExt  = '';
+        if ( $Param{Filename} =~ /(.*)\.+(.*)$/ ) {
+
+            # allow extensions up to 10 chars to avoid infinite loop below
+            if (length encode('UTF-8', $2) <= 10) {
+
+                $FileName = $1;
+                $FileExt  = '.' . $2;
+            }
+
+        }
+
+        if ( length $FileName ) {
+            my $ModifiedName;
+
+            # remove character by character starting from the end of the filename string
+            # untill we get acceptable 220 byte long filename size including extension
+            CHOPSTRING:
+            while (1) {
+
+                $ModifiedName = $FileName . $FileExt;
+
+                last CHOPSTRING if ( length encode( 'UTF-8', $ModifiedName ) < 220 );
+                chop $FileName;
+
+            }
+            $Param{Filename} = $ModifiedName;
+        }
     }
 
     return $Param{Filename};

commit 9746abd712fac985fb31d8930137efad893c3024

Fixed upload cache filename cleanup and filename cleanup optimization
Uploading attachments with long names didn't work correctly (extra
files created by OTRS were truncated incorrectly).

Filename cleanup routine was complicated and heavy (encoding,
trimmimg in loop, etc.).

This fixes both issues.

Fixes: e646974c93f19ed22e20a72ad595b6117150981f
Fixes: fe9b0c41bd1e21a27148643754abaaa2c0071bfa
Related: https://dev.ib.pl/ib/otrs/issues/118
Author-Change-Id: IB#1085514

Kernel/System/EmailParser.pm
@@ -720,21 +720,22 @@ sub PartsAttachments {
         my ($SubjectString) = $Part->as_string() =~ m/^Subject: ([^\n]*(\n[ \t][^\n]*)*)/m;
         my $Subject = $Self->_DecodeString( String => $SubjectString );
 
-        # trim whitespace
-        $Subject =~ s/^\s+|\n|\s+$//g;
-        if ( length($Subject) > 246 ) {
-            $Subject = substr( $Subject, 0, 246 );
-        }
+        if ( defined($Subject) && ( length($Subject) > 0 ) ) {
 
-        if ( $Subject eq '' ) {
-            $Self->{NoFilenamePartCounter}++;
-            $Subject = "Unbenannt-$Self->{NoFilenamePartCounter}";
+            # cleanup filename
+            $PartData{Filename} = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+                Filename => $Subject . '.eml',
+                Type     => 'Local',
+            );
         }
-        $PartData{Filename} = $Subject . '.eml';
+        else {
+            $Self->{NoFilenamePartCounter}++;
+            $PartData{Filename} = 'file-' . $Self->{NoFilenamePartCounter} . '.eml';
+        }
     }
     else {
         $Self->{NoFilenamePartCounter}++;
-        $PartData{Filename} = "file-$Self->{NoFilenamePartCounter}";
+        $PartData{Filename} = 'file-' . $Self->{NoFilenamePartCounter};
     }
 
     # parse/get Content-Id, Content-Location and Disposition for html email attachments

Kernel/System/Main.pm
@@ -259,34 +259,25 @@ sub FilenameCleanUp {
         $Param{Filename} =~ s/(\x{00C3}\x{009F}|\x{00DF})/ss/g;
         $Param{Filename} =~ s/-+/-/g;
 
-        # separate filename and extension
-        my $FileName = $Param{Filename};
-        my $FileExt  = '';
-        if ( $Param{Filename} =~ /(.*)\.+(.*)$/ ) {
+        # trim filename to 100 chars (=200 bytes in case of all UTF-8 chars) if longer
+        # to avoid exceeding filesystem limits (i.e. 255 byte filename length limit in XFS);
+        # must leave enought space for extra OTRS extensions (i.e. .ContentID, .content_type)
+        # that are suffixed without calling FilenameCleanUp()
+        if ( length($Param{Filename}) > 100 ) {
 
-            # allow extensions up to 10 chars to avoid infinite loop below
-            if (length encode('UTF-8', $2) <= 10) {
-                $FileName = $1;
-                $FileExt  = '.' . $2;
+            # if extension exists and is not longer than 50 chars
+            # than leave whole extension and trim filename part before
+            # extension only; trim whole filename otherwise
+
+            my $FileName = $Param{Filename};
+            my $FileExt  = '';
+            if ( $Param{Filename} =~ /(.*)\.+(.*)$/ ) {
+                if (length($2) <= 50) {
+                    $FileName = $1;
+                    $FileExt = '.' . $2;
+                }
             }
-
-        }
-
-        if ( length $FileName ) {
-            my $ModifiedName;
-
-            # remove character by character starting from the end of the filename string
-            # untill we get acceptable 220 byte long filename size including extension
-            CHOPSTRING:
-            while (1) {
-
-                $ModifiedName = $FileName . $FileExt;
-
-                last CHOPSTRING if ( length encode( 'UTF-8', $ModifiedName ) < 220 );
-                chop $FileName;
-
-            }
-            $Param{Filename} = $ModifiedName;
+            $Param{Filename} = substr($FileName, 0, 100-length($FileExt)) . $FileExt;
         }
     }
     else {
@@ -305,35 +296,25 @@ sub FilenameCleanUp {
             $Param{Filename} =~ s/[\x{2460}-\x{24FF}]/_/g;
         }
 
-        # separate filename and extension
-        my $FileName = $Param{Filename};
-        my $FileExt  = '';
-        if ( $Param{Filename} =~ /(.*)\.+(.*)$/ ) {
+        # trim filename to 100 chars (=200 bytes in case of all UTF-8 chars) if longer
+        # to avoid exceeding filesystem limits (i.e. 255 byte filename length limit in XFS);
+        # must leave enought space for extra OTRS extensions (i.e. .ContentID, .content_type)
+        # that are suffixed without calling FilenameCleanUp()
+        if ( length($Param{Filename}) > 100 ) {
 
-            # allow extensions up to 10 chars to avoid infinite loop below
-            if (length encode('UTF-8', $2) <= 10) {
+            # if extension exists and is not longer than 50 chars
+            # than leave whole extension and trim filename part before
+            # extension only; trim whole filename otherwise
 
-                $FileName = $1;
-                $FileExt  = '.' . $2;
+            my $FileName = $Param{Filename};
+            my $FileExt  = '';
+            if ( $Param{Filename} =~ /(.*)\.+(.*)$/ ) {
+                if (length($2) <= 50) {
+                    $FileName = $1;
+                    $FileExt = '.' . $2;
+                }
             }
-
-        }
-
-        if ( length $FileName ) {
-            my $ModifiedName;
-
-            # remove character by character starting from the end of the filename string
-            # untill we get acceptable 220 byte long filename size including extension
-            CHOPSTRING:
-            while (1) {
-
-                $ModifiedName = $FileName . $FileExt;
-
-                last CHOPSTRING if ( length encode( 'UTF-8', $ModifiedName ) < 220 );
-                chop $FileName;
-
-            }
-            $Param{Filename} = $ModifiedName;
+            $Param{Filename} = substr($FileName, 0, 100-length($FileExt)) . $FileExt;
         }
     }
 
@@ -382,8 +363,9 @@ sub FileRead {
 
         # filename clean up
         $Param{Filename} = $Self->FilenameCleanUp(
-            Filename => $Param{Filename},
-            Type     => $Param{Type} || 'Local',    # Local|Attachment|MD5
+            Filename        => $Param{Filename},
+            Type            => $Param{Type} || 'Local',    # Local|Attachment|MD5
+            NoFilenameClean => $Param{NoFilenameClean},
         );
         $Param{Location} = "$Param{Directory}/$Param{Filename}";
     }
@@ -609,8 +591,9 @@ sub FileDelete {
 
         # filename clean up
         $Param{Filename} = $Self->FilenameCleanUp(
-            Filename => $Param{Filename},
-            Type     => $Param{Type} || 'Local',    # Local|Attachment|MD5
+            Filename        => $Param{Filename},
+            Type            => $Param{Type} || 'Local',    # Local|Attachment|MD5
+            NoFilenameClean => $Param{NoFilenameClean},
         );
         $Param{Location} = "$Param{Directory}/$Param{Filename}";
     }
@@ -672,8 +655,9 @@ sub FileGetMTime {
 
         # filename clean up
         $Param{Filename} = $Self->FilenameCleanUp(
-            Filename => $Param{Filename},
-            Type     => $Param{Type} || 'Local',    # Local|Attachment|MD5
+            Filename        => $Param{Filename},
+            Type            => $Param{Type} || 'Local',    # Local|Attachment|MD5
+            NoFilenameClean => $Param{NoFilenameClean},
         );
         $Param{Location} = "$Param{Directory}/$Param{Filename}";
     }

Kernel/System/Web/UploadCache/FS.pm
@@ -126,34 +126,42 @@ sub FormIDAddFile {
     # get main object
     my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
 
+    $Param{Filename} = $MainObject->FilenameCleanUp(
+        Filename => $Param{Filename},
+    );
+
     # files must readable for creator
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}",
-        Content    => \$Param{Content},
-        Mode       => 'binmode',
-        Permission => '640',
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}",
+        Content         => \$Param{Content},
+        Mode            => 'binmode',
+        Permission      => '640',
+        NoFilenameClean => 1,
     );
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}.ContentType",
-        Content    => \$Param{ContentType},
-        Mode       => 'binmode',
-        Permission => '640',
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}.ContentType",
+        Content         => \$Param{ContentType},
+        Mode            => 'binmode',
+        Permission      => '640',
+        NoFilenameClean => 1,
     );
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}.ContentID",
-        Content    => \$ContentID,
-        Mode       => 'binmode',
-        Permission => '640',
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}.ContentID",
+        Content         => \$ContentID,
+        Mode            => 'binmode',
+        Permission      => '640',
+        NoFilenameClean => 1,
     );
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}.Disposition",
-        Content    => \$Disposition,
-        Mode       => 'binmode',
-        Permission => '644',
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}.Disposition",
+        Content         => \$Disposition,
+        Mode            => 'binmode',
+        Permission      => '640',
+        NoFilenameClean => 1,
     );
     return 1;
 }
@@ -189,20 +197,24 @@ sub FormIDRemoveFile {
     my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
 
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}",
+        Directory       => $Directory,
+        Filename        => "$File{Filename}",
+        NoFilenameClean => 1,
     );
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.ContentType",
+        Directory       => $Directory,
+        Filename        => "$File{Filename}.ContentType",
+        NoFilenameClean => 1,
     );
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.ContentID",
+        Directory       => $Directory,
+        Filename        => "$File{Filename}.ContentID",
+        NoFilenameClean => 1,
     );
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.Disposition",
+        Directory       => $Directory,
+        Filename        => "$File{Filename}.Disposition",
+        NoFilenameClean => 1,
     );
 
     return 1;
@@ -266,20 +278,23 @@ sub FormIDGetAllFilesData {
             }
         }
         my $Content = $MainObject->FileRead(
-            Location => $File,
-            Mode     => 'binmode',                                             # optional - binmode|utf8
+            Location        => $File,
+            Mode            => 'binmode',                                      # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$Content;
 
         my $ContentType = $MainObject->FileRead(
-            Location => "$File.ContentType",
-            Mode     => 'binmode',                                             # optional - binmode|utf8
+            Location        => "$File.ContentType",
+            Mode            => 'binmode',                                      # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentType;
 
         my $ContentID = $MainObject->FileRead(
-            Location => "$File.ContentID",
-            Mode     => 'binmode',                                             # optional - binmode|utf8
+            Location        => "$File.ContentID",
+            Mode            => 'binmode',                                      # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentID;
 
@@ -289,8 +304,9 @@ sub FormIDGetAllFilesData {
         }
 
         my $Disposition = $MainObject->FileRead(
-            Location => "$File.Disposition",
-            Mode     => 'binmode',                                             # optional - binmode|utf8
+            Location        => "$File.Disposition",
+            Mode            => 'binmode',                                      # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$Disposition;
 
@@ -372,14 +388,16 @@ sub FormIDGetAllFilesMeta {
         }
 
         my $ContentType = $MainObject->FileRead(
-            Location => "$File.ContentType",
-            Mode     => 'binmode',                                             # optional - binmode|utf8
+            Location        => "$File.ContentType",
+            Mode            => 'binmode',                                      # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentType;
 
         my $ContentID = $MainObject->FileRead(
-            Location => "$File.ContentID",
-            Mode     => 'binmode',                                             # optional - binmode|utf8
+            Location        => "$File.ContentID",
+            Mode            => 'binmode',                                      # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentID;
 
@@ -389,8 +407,9 @@ sub FormIDGetAllFilesMeta {
         }
 
         my $Disposition = $MainObject->FileRead(
-            Location => "$File.Disposition",
-            Mode     => 'binmode',                                             # optional - binmode|utf8
+            Location        => "$File.Disposition",
+            Mode            => 'binmode',                                      # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$Disposition;
 